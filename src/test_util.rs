06ea55c39358cbf90f29719c54fc14449e96c0d7
Ali Hasani <a.hassssani@gmail.com>
2020-05-31 18:17:38 +0430

    ignore line_src field

--- src/test_util.rs
+++ src/test_util.rs
@@ -1,8 +1,8 @@
 // Copyright 2020 the Deno authors. All rights reserved. MIT license.
 
-use crate::rules::LintRule;
+use crate::rules::{LintDiagnostic, LintRule};
 use crate::Linter;
-use serde_json::Value;
+use serde_json::{json, Value};
 
 pub fn test_lint(
   filename: &str,
@@ -14,6 +14,25 @@ pub fn test_lint(
   let diagnostics = linter
     .lint(filename.to_string(), source_code.to_string(), rules)
     .expect("Failed to lint");
-  let serialized_diagnostics = serde_json::to_value(diagnostics).unwrap();
+
+  // TODO(@disizali) refactor this to a better approach.
+  // it's a temporary solution for ignoring line_src field.
+  let serialized_diagnostics;
+  if !diagnostics.is_empty() {
+    let mut ignored_line_src_diagnostics = vec![];
+    for diagnostic in &diagnostics {
+      let LintDiagnostic {
+        code,
+        message,
+        location,
+        ..
+      } = diagnostic;
+      ignored_line_src_diagnostics
+        .push(json!({"code":code,"message":message,"location":location}))
+    }
+    serialized_diagnostics = serde_json::to_value(ignored_line_src_diagnostics).unwrap();
+  } else {
+    serialized_diagnostics = serde_json::to_value(diagnostics).unwrap();
+  }
   assert_eq!(serialized_diagnostics, expected_diagnostics);
 }
