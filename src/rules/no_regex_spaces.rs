1423525d95e656ee73d2f5ddb35f557a08900d93
Ali Hasani <a.hassssani@gmail.com>
2020-06-01 23:10:58 +0430

    add CallExpr

--- src/rules/no_regex_spaces.rs
+++ src/rules/no_regex_spaces.rs
@@ -1,7 +1,7 @@
 // Copyright 2020 the Deno authors. All rights reserved. MIT license.
 use super::Context;
 use super::LintRule;
-use swc_ecma_ast::{Expr, Lit, NewExpr, Regex};
+use swc_ecma_ast::{CallExpr, Expr, ExprOrSuper, Lit, NewExpr, Regex};
 use swc_ecma_visit::Node;
 use swc_ecma_visit::Visit;
 
@@ -64,6 +64,38 @@ impl Visit for NoRegexSpacesVisitor {
               "more than one consecutive spaces in RegExp is not allowed",
             );
           }
+        }
+      }
+    }
+  }
+
+  fn visit_call_expr(&mut self, call_expr: &CallExpr, _parent: &dyn Node) {
+    if let ExprOrSuper::Expr(expr) = &call_expr.callee {
+      if let Expr::Ident(ident) = expr.as_ref() {
+        let name = ident.sym.to_string();
+        if name != "RegExp" {
+          return;
+        } else {
+          if !call_expr.args.is_empty() {
+            if let Some(first_arg) = call_expr.args.get(0) {
+              let regex_literal =
+                if let Expr::Lit(Lit::Str(literal)) = &*first_arg.expr {
+                  &literal.value
+                } else if let Expr::Lit(Lit::Regex(regex)) = &*first_arg.expr {
+                  &regex.exp
+                } else {
+                  return;
+                };
+
+              if regex_literal.matches("  ").count() > 0 {
+                self.context.add_diagnostic(
+                  call_expr.span,
+                  "noRegexSpaces",
+                  "more than one consecutive spaces in RegExp is not allowed",
+                );
+              }
+            }
+          };
         }
       }
     }
@@ -132,17 +164,68 @@ var re = new RegExp(/a  z/);
     test_lint(
       "no_regex_spaces",
       r#"
-var re = new RegExp(/a {2}z/);
+var re = new RegExp(/a  z/);
     "#,
       vec![NoRegexSpaces::new()],
-      json!([]),
+      json!([{
+        "code": "noRegexSpaces",
+        "message": "more than one consecutive spaces in RegExp is not allowed",
+        "location": {
+          "filename": "no_regex_spaces",
+          "line": 2,
+          "col": 9,
+        }
+      }]),
     );
+  }
 
+  #[test]
+  fn no_regex_spaces_ok() {
     test_lint(
       "no_regex_spaces",
       r#"
-var re = /a {30}z/;
-  "#,
+          var foo = /foo/;
+          var foo = RegExp('foo')
+          var foo = / /;
+          var foo = RegExp(' ')
+          var foo = / a b c d /;
+          var foo = /bar {3}baz/g;
+          var foo = RegExp('bar {3}baz','g')
+          var foo = new RegExp('bar {3}baz')
+          var foo = /bar\t\t\tbaz/;
+          var foo = RegExp('bar\t\t\tbaz');
+          var foo = new RegExp('bar\t\t\tbaz');
+          var RegExp = function() {}; var foo = new RegExp('bar   baz');
+          var RegExp = function() {}; var foo = RegExp('bar   baz');
+          var foo = /  +/;
+          var foo = /  ?/;
+          var foo = /  */;
+          var foo = /  {2}/;
+          var foo = /bar \\ baz/;
+          var foo = /bar\\ \\ baz/;
+          var foo = /bar \\u0020 baz/;
+          var foo = /bar\\u0020\\u0020baz/;
+          var foo = new RegExp('bar \\ baz')
+          var foo = new RegExp('bar\\ \\ baz')
+          var foo = new RegExp('bar \\\\ baz')
+          var foo = new RegExp('bar \\u0020 baz')
+          var foo = new RegExp('bar\\u0020\\u0020baz')
+          var foo = new RegExp('bar \\\\u0020 baz')
+          /*
+          var foo = /[  ]/;
+          var foo = /[   ]/;
+          var foo = / [  ] /;
+          var foo = / [  ] [  ] /;
+          var foo = new RegExp('[  ]');
+          var foo = new RegExp('[   ]');
+          var foo = new RegExp(' [  ] ');
+          var foo = RegExp(' [  ] [  ] ');
+          var foo = new RegExp(' \\[   ');
+          var foo = new RegExp(' \\[   \\] ');
+          var foo = new RegExp('[  ');
+          var foo = new RegExp('{  ','u');
+          */
+        "#,
       vec![NoRegexSpaces::new()],
       json!([]),
     );
