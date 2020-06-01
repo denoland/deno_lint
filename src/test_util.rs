0d2309d940c2d59ba277a18fc919d1fd6a5cab3b
Luca Casonato <lucacasonato@yahoo.com>
2020-06-01 21:53:02 +0200

    Remove test_util function

--- src/test_util.rs
+++ src/test_util.rs
@@ -4,41 +4,6 @@ use crate::linter::LintDiagnostic;
 use crate::rules::LintRule;
 use crate::Linter;
 
-use serde_json::{json, Value};
-
-pub fn test_lint(
-  filename: &str,
-  source_code: &str,
-  rules: Vec<Box<dyn LintRule>>,
-  expected_diagnostics: Value,
-) {
-  let mut linter = Linter::default();
-  let diagnostics = linter
-    .lint(filename.to_string(), source_code.to_string(), rules)
-    .expect("Failed to lint");
-
-  // TODO(@disizali) refactor this to a better approach.
-  // it's a temporary solution for ignoring line_src field.
-  let serialized_diagnostics = if !diagnostics.is_empty() {
-    let mut ignored_line_src_diagnostics = vec![];
-    for diagnostic in &diagnostics {
-      let LintDiagnostic {
-        code,
-        message,
-        location,
-        ..
-      } = diagnostic;
-      ignored_line_src_diagnostics
-        .push(json!({"code":code,"message":message,"location":location}))
-    }
-    serde_json::to_value(ignored_line_src_diagnostics).unwrap()
-  } else {
-    serde_json::to_value(diagnostics).unwrap()
-  };
-
-  assert_eq!(serialized_diagnostics, expected_diagnostics);
-}
-
 fn lint<T: LintRule + 'static>(source: &str) -> Vec<LintDiagnostic> {
   let mut linter = Linter::default();
   let rule = T::new();
@@ -85,7 +50,6 @@ pub fn assert_lint_ok_n<T: LintRule + 'static>(cases: Vec<&str>) {
   }
 }
 
-#[allow(dead_code)]
 pub fn assert_lint_err<T: LintRule + 'static>(
   source: &str,
   code: &str,
@@ -105,7 +69,6 @@ pub fn assert_lint_err_on_line<T: LintRule + 'static>(
   assert_diagnostic(&diagnostics[0], code, line, col);
 }
 
-#[allow(dead_code)]
 pub fn assert_lint_err_n<T: LintRule + 'static>(
   source: &str,
   expected: Vec<(&str, usize)>,
