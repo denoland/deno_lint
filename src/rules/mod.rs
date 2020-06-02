7cd6ca38cc6392e9ff8a6eaf8a7d4cd0aa5f417e
Ali Hasani <a.hassssani@gmail.com>
2020-06-02 15:12:42 +0430

    Add noOctal rule (#79)

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -28,6 +28,7 @@ mod no_explicit_any;
 mod no_func_assign;
 mod no_new_symbol;
 mod no_obj_call;
+mod no_octal;
 mod no_prototype_builtins;
 mod no_setter_return;
 mod no_sparse_array;
@@ -87,6 +88,7 @@ pub fn get_all_rules() -> Vec<Box<dyn LintRule>> {
     single_var_declarator::SingleVarDeclarator::new(),
     use_isnan::UseIsNaN::new(),
     valid_typeof::ValidTypeof::new(),
+    no_octal::NoOctal::new(),
     no_obj_call::NoObjCall::new(),
   ]
 }
