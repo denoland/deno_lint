2bf5b480715635acdc6c495affdf9c783b773688
Ali Hasani <a.hassssani@gmail.com>
2020-06-02 05:44:16 +0430

    add no_octal rule

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -27,6 +27,7 @@ mod no_ex_assign;
 mod no_explicit_any;
 mod no_func_assign;
 mod no_new_symbol;
+mod no_octal;
 mod no_prototype_builtins;
 mod no_setter_return;
 mod no_sparse_array;
@@ -86,5 +87,6 @@ pub fn get_all_rules() -> Vec<Box<dyn LintRule>> {
     single_var_declarator::SingleVarDeclarator::new(),
     use_isnan::UseIsNaN::new(),
     valid_typeof::ValidTypeof::new(),
+    no_octal::NoOctal::new(),
   ]
 }
