39161dedec81ad13ec76f98a07912b3bd22f1c5a
Ali Hasani <a.hassssani@gmail.com>
2020-06-02 17:11:30 +0430

    lazystatic - comments - more tests

--- src/rules/no_empty_character_class.rs
+++ src/rules/no_empty_character_class.rs
@@ -35,10 +35,25 @@ impl NoEmptyCharacterClassVisitor {
 impl Visit for NoEmptyCharacterClassVisitor {
   fn visit_regex(&mut self, regex: &Regex, _parent: &dyn Node) {
     let regex_literal = format!("/{}/", regex.exp);
-    let rule_regex =
-      regex::Regex::new(r"^/([^\[]|\.|\[([^\\\]]|\\.)+\])*/[gimuys]*$")
-        .unwrap();
-    if !rule_regex.is_match(&regex_literal) {
+    lazy_static! {
+      /* reference : [eslint no-empty-character-class](https://github.com/eslint/eslint/blob/master/lib/rules/no-empty-character-class.js#L13)
+      * plain-English description of the following regexp:
+      * 0. `^` fix the match at the beginning of the string
+      * 1. `\/`: the `/` that begins the regexp
+      * 2. `([^\\[]|\\.|\[([^\\\]]|\\.)+\])*`: regexp contents; 0 or more of the following
+      * 2.0. `[^\\[]`: any character that's not a `\` or a `[` (anything but escape sequences and character classes)
+      * 2.1. `\\.`: an escape sequence
+      * 2.2. `\[([^\\\]]|\\.)+\]`: a character class that isn't empty
+      * 3. `\/` the `/` that ends the regexp
+      * 4. `[gimuy]*`: optional regexp flags
+      * 5. `$`: fix the match at the end of the string
+      */
+      static ref RULE_REGEX: regex::Regex = regex::Regex::new(
+        r"(?u)^/([^\\\[]|\\\.|\[([^\\\]]|\\.)+\])*/[gimuys]*$"
+      )
+      .unwrap();
+    }
+    if !RULE_REGEX.is_match(&regex_literal) {
       self.context.add_diagnostic(
         regex.span,
         "noEmptyCharacterClass",
@@ -54,19 +69,52 @@ mod tests {
   use crate::test_util::*;
 
   #[test]
+  fn no_empty_character_class() {
+    assert_lint_err::<NoEmptyCharacterClass>(r#"const foo = /^abc[]/;"#, 12);
+    assert_lint_err::<NoEmptyCharacterClass>(r#"const foo = /foo[]bar/;"#, 12);
+    assert_lint_err::<NoEmptyCharacterClass>(r#"const foo = /[]]/;"#, 12);
+    assert_lint_err::<NoEmptyCharacterClass>(r#"const foo = /\[[]/;"#, 12);
+    assert_lint_err::<NoEmptyCharacterClass>(
+      r#"const foo = /\\[\\[\\]a-z[]/;"#,
+      12,
+    );
+  }
+
+  #[test]
   fn no_empty_character_class_match() {
     assert_lint_err::<NoEmptyCharacterClass>(r#"/^abc[]/.test("abcdefg");"#, 0);
+    assert_lint_err::<NoEmptyCharacterClass>(
+      r#"if (foo.match(/^abc[]/)) {}"#,
+      14,
+    );
   }
+
   #[test]
   fn no_empty_character_class_test() {
     assert_lint_err::<NoEmptyCharacterClass>(
       r#""abcdefg".match(/^abc[]/);"#,
       16,
     );
+    assert_lint_err::<NoEmptyCharacterClass>(
+      r#"if (/^abc[]/.test(foo)) {}"#,
+      4,
+    );
   }
 
   #[test]
-  fn empty_character_class_string() {
-    assert_lint_ok::<NoEmptyCharacterClass>(r#"new RegExp("^abc[]");"#);
+  fn no_empty_character_class_valid() {
+    assert_lint_ok::<NoEmptyCharacterClass>(
+      r#"
+    const foo = /^abc[a-zA-Z]/;
+    const regExp = new RegExp("^abc[]");
+    const foo = /^abc/;
+    const foo = /[\\[]/;
+    const foo = /[\\]]/;
+    const foo = /[a-zA-Z\\[]/;
+    const foo = /[[]/;
+    const foo = /[\\[a-z[]]/;
+    const foo = /[\-\[\]\/\{\}\(\)\*\+\?\.\\^\$\|]/g;
+    "#,
+    );
   }
 }
