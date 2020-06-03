cdf8c1d65d4f51af028ef9ed0a509096e9a2143f
Ali Hasani <a.hassssani@gmail.com>
2020-06-04 04:20:27 +0430

    fix octal with regex

--- src/rules/no_octal.rs
+++ src/rules/no_octal.rs
@@ -35,10 +35,7 @@ impl NoOctalVisitor {
 impl Visit for NoOctalVisitor {
   fn visit_number(&mut self, literal_num: &Number, _parent: &dyn Node) {
     lazy_static! {
-      static ref OCTAL: regex::Regex = regex::Regex::new(
-        r"^0[0-9]"
-      )
-      .unwrap();
+      static ref OCTAL: regex::Regex = regex::Regex::new(r"^0[0-9]").unwrap();
     }
 
     let raw_number = self
@@ -47,7 +44,7 @@ impl Visit for NoOctalVisitor {
       .span_to_snippet(literal_num.span)
       .expect("error in loading snippet");
 
-  if OCTAL.is_match(&raw_number) {
+    if OCTAL.is_match(&raw_number) {
       self.context.add_diagnostic(
         literal_num.span,
         "no-octal",
@@ -73,17 +70,7 @@ mod tests {
   }
 
   #[test]
-  fn test_new_normal_number() {
-    assert_lint_ok::<NoOctal>("7");
-  }
-
-  #[test]
-  fn test_string_octal_number() {
-    assert_lint_ok::<NoOctal>("\"07\"");
-  }
-
-  #[test]
-  fn test_zero_number() {
-    assert_lint_ok::<NoOctal>("0");
+  fn test_octals_valid() {
+    assert_lint_ok_n::<NoOctal>(vec!["7", "\"07\"", "0x08", "-0.01"]);
   }
 }
