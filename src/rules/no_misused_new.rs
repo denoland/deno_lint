79eab3d3adad7f9e8ed1f5c23a0e75eab6fb445b
Ardy Febriansyah <ardyfeb0@gmail.com>
2020-06-07 04:12:18 +0700

    fix lint

--- src/rules/no_misused_new.rs
+++ src/rules/no_misused_new.rs
@@ -1,142 +1,133 @@
 use super::Context;
 use super::LintRule;
-use swc_ecma_visit::Node;
-use swc_ecma_visit::Visit;
 use swc_atoms::JsWord;
 use swc_ecma_ast::{
-    Module,
-    TsTypeElement::{TsMethodSignature, TsConstructSignatureDecl},
-    Expr,
-    ClassDecl,
-    TsTypeAnn,
-    Ident,
-    TsType,
-    TsEntityName,
-    ClassMember,
-    TsInterfaceDecl,
+  ClassDecl, ClassMember, Expr, Ident, Module, TsEntityName, TsInterfaceDecl,
+  TsType, TsTypeAnn,
+  TsTypeElement::{TsConstructSignatureDecl, TsMethodSignature},
 };
+use swc_ecma_visit::Node;
+use swc_ecma_visit::Visit;
 
 pub struct NoMisusedNew;
 
 impl LintRule for NoMisusedNew {
-    fn new() -> Box<Self> {
-        Box::new(NoMisusedNew)
-    }
+  fn new() -> Box<Self> {
+    Box::new(NoMisusedNew)
+  }
 
-    fn lint_module(&self, context: Context, module: Module) {
-        let mut visitor = NoMisusedNewVisitor::new(context);
-        visitor.visit_module(&module, &module);
-    }
+  fn lint_module(&self, context: Context, module: Module) {
+    let mut visitor = NoMisusedNewVisitor::new(context);
+    visitor.visit_module(&module, &module);
+  }
 
-    fn code(&self) -> &'static str {
-        "noMisusedNew"
-    }
+  fn code(&self) -> &'static str {
+    "noMisusedNew"
+  }
 }
 
 pub struct NoMisusedNewVisitor {
-    context: Context,
+  context: Context,
 }
 
 impl NoMisusedNewVisitor {
-    fn new(context: Context) -> Self {
-        Self { context }
-    }
+  fn new(context: Context) -> Self {
+    Self { context }
+  }
 
-    fn match_parent_type(&self, parent: &Ident, return_type: &TsTypeAnn) -> bool {
-        match &*return_type.type_ann {
-            TsType::TsTypeRef(type_ref) => {
-                if let TsEntityName::Ident(ident) = &type_ref.type_name {
-                    return ident.sym == parent.sym;
-                }
-            }
-            _ => {}
+  fn match_parent_type(&self, parent: &Ident, return_type: &TsTypeAnn) -> bool {
+    match &*return_type.type_ann {
+      TsType::TsTypeRef(type_ref) => {
+        if let TsEntityName::Ident(ident) = &type_ref.type_name {
+          return ident.sym == parent.sym;
         }
-
-        return false;
+      }
+      _ => {}
     }
+
+    return false;
+  }
 }
 
 impl Visit for NoMisusedNewVisitor {
-    fn visit_ts_interface_decl(&mut self, n: &TsInterfaceDecl, _parent: &dyn Node) {
-        for member in &n.body.body {
-            match &member {
-                TsMethodSignature(signature) | TsConstructSignatureDecl(signature) => {
-                    if let Expr::Ident(ident) = &*signature.key {
-                        if JsWord::from("constructor") == ident.sym &&
-                            signature.type_ann.is_some() &&
-                            self.match_parent_type(
-                                &n.id,
-                                &signature.type_ann.as_ref().unwrap(),
-                            ) {
-
-                            // constructor
-                            self.context.add_diagnostic(
-                                signature.span,
-                                "noMisusedNew",
-                                "Interfaces cannot be constructed, only classes",
-                            );
-                        }
-                    }
-                }
-                _ => {}
+  fn visit_ts_interface_decl(
+    &mut self,
+    n: &TsInterfaceDecl,
+    _parent: &dyn Node,
+  ) {
+    for member in &n.body.body {
+      match &member {
+        TsMethodSignature(signature) | TsConstructSignatureDecl(signature) => {
+          if let Expr::Ident(ident) = &*signature.key {
+            if JsWord::from("constructor") == ident.sym
+              && signature.type_ann.is_some()
+              && self
+                .match_parent_type(&n.id, &signature.type_ann.as_ref().unwrap())
+            {
+              // constructor
+              self.context.add_diagnostic(
+                signature.span,
+                "noMisusedNew",
+                "Interfaces cannot be constructed, only classes",
+              );
             }
+          }
         }
+        _ => {}
+      }
     }
+  }
 
-    fn visit_class_decl(&mut self, expr: &ClassDecl, _parent: &dyn Node) {
-        for member in &expr.class.body {
-            match member {
-                ClassMember::Method(method) => {
-                    if method.function.return_type.is_some() &&
-                        self.match_parent_type(
-                            &expr.ident,
-                            &method.function.return_type.as_ref().unwrap(),
-                        ) {
-
-                        // new
-                        self.context.add_diagnostic(
-                            method.span,
-                            "noMisusedNew",
-                            "Class cannot have method named `new`.",
-                        );
-                    }
-                }
-                _ => {}
-            }
+  fn visit_class_decl(&mut self, expr: &ClassDecl, _parent: &dyn Node) {
+    for member in &expr.class.body {
+      match member {
+        ClassMember::Method(method) => {
+          if method.function.return_type.is_some()
+            && self.match_parent_type(
+              &expr.ident,
+              &method.function.return_type.as_ref().unwrap(),
+            )
+          {
+            // new
+            self.context.add_diagnostic(
+              method.span,
+              "noMisusedNew",
+              "Class cannot have method named `new`.",
+            );
+          }
         }
+        _ => {}
+      }
     }
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::test_util::*;
+  use super::*;
+  use crate::test_util::*;
 
-    #[test]
-    fn no_misused_new_test() {
-        assert_lint_err_on_line::<NoMisusedNew>(
-            r#"
+  #[test]
+  fn no_misused_new_test() {
+    assert_lint_err_on_line::<NoMisusedNew>(
+      r#"
 interface I {
     constructor(): I
 }
            "#,
-            3,
-            4,
-        );
-        assert_lint_err_on_line::<NoMisusedNew>(
-            r#"
+      3,
+      4,
+    );
+    assert_lint_err_on_line::<NoMisusedNew>(
+      r#"
 class C {
     new(): C
 }
            "#,
-            3,
-            4,
-        );
-        assert_lint_ok::<NoMisusedNew>(
-            "class C { new(): void }"
-        );
-        assert_lint_ok::<NoMisusedNew>(
-            "interface IC { constructor(): void }"
-        );
-    }
+      3,
+      4,
+    );
+    assert_lint_ok::<NoMisusedNew>("class C { new(): void }");
+    assert_lint_ok::<NoMisusedNew>("interface IC { constructor(): void }");
+  }
 }
