7986ca0cf0480698c13126aa776a2f4c58ff1f99
Ali Hasani <a.hassssani@gmail.com>
2020-06-09 19:50:49 +0430

    add no this alias

--- src/rules/no_this_alias.rs
+++ src/rules/no_this_alias.rs
@@ -1,7 +1,7 @@
 // Copyright 2020 the Deno authors. All rights reserved. MIT license.
 use super::Context;
 use super::LintRule;
-use swc_ecma_ast::{Expr, VarDecl, ArrowExpr, Function };
+use swc_ecma_ast::{ArrowExpr, Expr, Function, VarDecl};
 use swc_ecma_visit::Node;
 use swc_ecma_visit::Visit;
 
@@ -35,7 +35,9 @@ impl NoThisAliasVisitor {
 impl Visit for NoThisAliasVisitor {
   fn visit_var_decl(&mut self, var_decl: &VarDecl, _parent: &dyn Node) {
     if let Some(init) = &var_decl.decls[0].init {
-      if let Expr::This(_) = **init {
+      if let Expr::Arrow(arrow) = &**init {
+        self.visit_arrow_expr(&arrow, _parent);
+      } else if let Expr::This(_) = &**init {
         self.context.add_diagnostic(
           var_decl.span,
           "no-this-alias",
@@ -45,12 +47,24 @@ impl Visit for NoThisAliasVisitor {
     }
   }
 
-  fn visit_arrow_expr(&mut self, arrow_expr: &ArrowExpr, _parent: &dyn Node){
-    println!("arrow : {:?}",arrow_expr);
+  fn visit_arrow_expr(&mut self, arrow_expr: &ArrowExpr, _parent: &dyn Node) {
+    self.visit_block_stmt_or_expr(&arrow_expr.body, _parent);
   }
-  
-  fn visit_function(&mut self, fnn: &Function, _parent: &dyn Node){
-    println!("fn : {:?}",fnn);
+
+  fn visit_expr_stmt(
+    &mut self,
+    expr: &swc_ecma_ast::ExprStmt,
+    _parent: &dyn Node,
+  ) {
+    if let Expr::Arrow(arrow) = &*expr.expr {
+      self.visit_arrow_expr(arrow, _parent);
+    }
+  }
+
+  fn visit_function(&mut self, function: &Function, _parent: &dyn Node) {
+    if let Some(stmt) = &function.body {
+      self.visit_block_stmt(stmt, _parent);
+    }
   }
 }
 
@@ -60,17 +74,50 @@ mod tests {
   use crate::test_util::*;
 
   #[test]
-  fn test_literal_octal() {
-    assert_lint_err::<NoThisAlias>("07", 0);
+  fn no_this_alias_valid() {
+    assert_lint_ok::<NoThisAlias>("const self = foo(this);");
+    assert_lint_ok::<NoThisAlias>("const self = foo(this);");
+    assert_lint_ok::<NoThisAlias>("const self = foo(this);");
   }
 
   #[test]
-  fn test_operand_octal() {
-    assert_lint_err::<NoThisAlias>("let x = 7 + 07", 12);
-  }
+  fn no_this_alias_invalid() {
+    assert_lint_err::<NoThisAlias>("const self = this;", 0);
+    assert_lint_err::<NoThisAlias>("const { props, state } = this;", 0);
+    assert_lint_err_on_line_n::<NoThisAlias>(
+      "
+var unscoped = this;
 
-  #[test]
-  fn test_octals_valid() {
-    assert_lint_ok_n::<NoThisAlias>(vec!["7", "\"07\"", "0x08", "-0.01"]);
+function testFunction() {
+  let inFunction = this;
+}
+
+const testLambda = () => {
+  const inLambda = this;
+};",
+      vec![(2, 0), (5, 2), (9, 2)],
+    );
+    assert_lint_err_on_line_n::<NoThisAlias>(
+      "
+class TestClass {
+  constructor() {
+    const inConstructor = this;
+    const asThis: this = this;
+      
+    const asString = 'this';
+    const asArray = [this];
+    const asArrayString = ['this'];
+  }
+      
+  public act(scope: this = this) {
+    const inMemberFunction = this;
+    const { act } = this;
+    const { act, constructor } = this;
+    const [foo] = this;
+    const [foo, bar] = this;
+  }
+}",
+      vec![(4, 4), (5, 4), (13, 4), (14, 4), (15, 4), (16, 4), (17, 4)],
+    );
   }
 }
