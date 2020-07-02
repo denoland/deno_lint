da63799ace665959b9913025af5f87d7e83c74e6
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-07-02 18:51:54 +0200

    finish

--- src/rules/no_extra_non_null_assertion.rs
+++ src/rules/no_extra_non_null_assertion.rs
@@ -1,10 +1,11 @@
 // Copyright 2020 the Deno authors. All rights reserved. MIT license.
 use super::Context;
 use super::LintRule;
-use swc_ecma_ast::TsNonNullExpr;
+use swc_common::Span;
+use swc_ecma_ast::Expr;
 use swc_ecma_ast::ExprOrSuper;
 use swc_ecma_ast::OptChainExpr;
-use swc_ecma_ast::Expr;
+use swc_ecma_ast::TsNonNullExpr;
 use swc_ecma_visit::Node;
 use swc_ecma_visit::Visit;
 
@@ -21,7 +22,6 @@ impl LintRule for NoExtraNonNullAssertion {
 
   fn lint_module(&self, context: Context, module: swc_ecma_ast::Module) {
     let mut visitor = NoExtraNonNullAssertionVisitor::new(context);
-    eprintln!("module {:#?}", module);
     visitor.visit_module(&module, &module);
   }
 }
@@ -34,35 +34,56 @@ impl NoExtraNonNullAssertionVisitor {
   pub fn new(context: Context) -> Self {
     Self { context }
   }
+
+  fn add_diagnostic(&mut self, span: Span) {
+    self.context.add_diagnostic(
+      span,
+      "no-extra-non-null-assertion",
+      "Extra non-null assertion is forbidden",
+    );
+  }
+
+  fn check_expr_for_nested_non_null_assert(&mut self, span: Span, expr: &Expr) {
+    match expr {
+      Expr::TsNonNull(_) => self.add_diagnostic(span),
+      Expr::Paren(paren_expr) => {
+        self.check_expr_for_nested_non_null_assert(span, &*paren_expr.expr)
+      }
+      _ => {}
+    }
+  }
 }
 
 impl Visit for NoExtraNonNullAssertionVisitor {
-  fn visit_ts_non_null_expr(&mut self, ts_non_null_expr: &TsNonNullExpr, parent: &dyn Node) {
-    if let Expr::TsNonNull(_non_null_expr) = &*ts_non_null_expr.expr {
-      self.context.add_diagnostic(
-        ts_non_null_expr.span,
-        "no-extra-non-null-assertion",
-        "Extra non-null assertion is forbidden",
-      );
-    }
+  fn visit_ts_non_null_expr(
+    &mut self,
+    ts_non_null_expr: &TsNonNullExpr,
+    parent: &dyn Node,
+  ) {
+    self.check_expr_for_nested_non_null_assert(
+      ts_non_null_expr.span,
+      &*ts_non_null_expr.expr,
+    );
     swc_ecma_visit::visit_ts_non_null_expr(self, ts_non_null_expr, parent);
   }
 
-  fn visit_opt_chain_expr(&mut self, opt_chain_expr: &OptChainExpr, parent: &dyn Node) {
-    eprintln!("opt chain expr {:#?}", opt_chain_expr);
+  fn visit_opt_chain_expr(
+    &mut self,
+    opt_chain_expr: &OptChainExpr,
+    parent: &dyn Node,
+  ) {
+    let maybe_expr_or_super = match &*opt_chain_expr.expr {
+      Expr::Member(member_expr) => Some(&member_expr.obj),
+      Expr::Call(call_expr) => Some(&call_expr.callee),
+      _ => None,
+    };
 
-    if let Expr::Member(member_expr) = &*opt_chain_expr.expr {
-      if let ExprOrSuper::Expr(expr) = &member_expr.obj {
-        if let Expr::TsNonNull(_non_null_expr) = &**expr {
-          self.context.add_diagnostic(
-            opt_chain_expr.span,
-            "no-extra-non-null-assertion",
-            "Extra non-null assertion is forbidden",
-          );
-        }
+    if let Some(expr_or_super) = maybe_expr_or_super {
+      if let ExprOrSuper::Expr(expr) = &expr_or_super {
+        self.check_expr_for_nested_non_null_assert(opt_chain_expr.span, expr);
       }
     }
-    
+
     swc_ecma_visit::visit_opt_chain_expr(self, opt_chain_expr, parent);
   }
 }
@@ -74,18 +95,49 @@ mod tests {
 
   #[test]
   fn no_extra_non_null_assertion_ok() {
-    assert_lint_ok::<NoExtraNonNullAssertion>(r#"const foo: { str: string } | null = null; const bar = foo!.str;"#);
-    assert_lint_ok::<NoExtraNonNullAssertion>(r#"function foo() { return "foo"; }"#);
-    assert_lint_ok::<NoExtraNonNullAssertion>(r#"function foo(bar: undefined | string) { return bar!; }"#);
-    assert_lint_ok::<NoExtraNonNullAssertion>(r#"function foo(bar?: { str: string }) { return bar?.str; }"#);
+    assert_lint_ok::<NoExtraNonNullAssertion>(
+      r#"const foo: { str: string } | null = null; const bar = foo!.str;"#,
+    );
+    assert_lint_ok::<NoExtraNonNullAssertion>(
+      r#"function foo() { return "foo"; }"#,
+    );
+    assert_lint_ok::<NoExtraNonNullAssertion>(
+      r#"function foo(bar: undefined | string) { return bar!; }"#,
+    );
+    assert_lint_ok::<NoExtraNonNullAssertion>(
+      r#"function foo(bar?: { str: string }) { return bar?.str; }"#,
+    );
   }
 
   #[test]
   fn no_extra_non_null_assertion_err() {
-    // assert_lint_err::<NoExtraNonNullAssertion>(r#"const foo: { str: string } | null = null; const bar = foo!!.str;"#, 54);
-    // assert_lint_err::<NoExtraNonNullAssertion>(r#"function foo(bar: undefined | string) { return bar!!; }"#, 47);
-    assert_lint_err::<NoExtraNonNullAssertion>(r#"function foo(bar?: { str: string }) { return bar!?.str; }"#, 45);
-    assert_lint_err::<NoExtraNonNullAssertion>(r#"function foo(bar?: { str: string }) { return (bar!)!.str; }"#, 45);
-    // assert_lint_err::<NoExtraNonNullAssertion>(r#"function foo(bar?: { str: string }) { return (bar!)?.str; }"#, 45);
+    assert_lint_err::<NoExtraNonNullAssertion>(
+      r#"const foo: { str: string } | null = null; const bar = foo!!.str;"#,
+      54,
+    );
+    assert_lint_err::<NoExtraNonNullAssertion>(
+      r#"function foo(bar: undefined | string) { return bar!!; }"#,
+      47,
+    );
+    assert_lint_err::<NoExtraNonNullAssertion>(
+      r#"function foo(bar?: { str: string }) { return bar!?.str; }"#,
+      45,
+    );
+    assert_lint_err::<NoExtraNonNullAssertion>(
+      r#"function foo(bar?: { str: string }) { return (bar!)!.str; }"#,
+      45,
+    );
+    assert_lint_err::<NoExtraNonNullAssertion>(
+      r#"function foo(bar?: { str: string }) { return (bar!)?.str; }"#,
+      45,
+    );
+    assert_lint_err::<NoExtraNonNullAssertion>(
+      r#"function foo(bar?: { str: string }) { return bar!?.(); }"#,
+      45,
+    );
+    assert_lint_err::<NoExtraNonNullAssertion>(
+      r#"function foo(bar?: { str: string }) { return (bar!)?.(); }"#,
+      45,
+    );
   }
 }
