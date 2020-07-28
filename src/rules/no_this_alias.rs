3cc7cefc20c98197d137aeb3d86606413a8770be
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-07-28 17:58:48 +0200

    fix: no-this-alias rule

--- src/rules/no_this_alias.rs
+++ src/rules/no_this_alias.rs
@@ -2,7 +2,7 @@
 use super::Context;
 use super::LintRule;
 use crate::swc_ecma_ast;
-use crate::swc_ecma_ast::{ArrowExpr, Expr, Function, VarDecl};
+use crate::swc_ecma_ast::{ArrowExpr, Expr, Function, Pat, VarDecl};
 use swc_ecma_visit::Node;
 use swc_ecma_visit::Visit;
 
@@ -37,15 +37,19 @@ impl NoThisAliasVisitor {
 
 impl Visit for NoThisAliasVisitor {
   fn visit_var_decl(&mut self, var_decl: &VarDecl, _parent: &dyn Node) {
-    if let Some(init) = &var_decl.decls[0].init {
-      if let Expr::Arrow(arrow) = &**init {
-        self.visit_arrow_expr(&arrow, _parent);
-      } else if let Expr::This(_) = &**init {
-        self.context.add_diagnostic(
-          var_decl.span,
-          "no-this-alias",
-          "assign `this` to declare a value is not allowed",
-        );
+    for decl in &var_decl.decls {
+      if let Some(init) = &decl.init {
+        if let Expr::Arrow(arrow) = &**init {
+          self.visit_arrow_expr(&arrow, _parent);
+        } else if let Expr::This(_) = &**init {
+          if let Pat::Ident(_ident) = &decl.name {
+            self.context.add_diagnostic(
+              var_decl.span,
+              "no-this-alias",
+              "assign `this` to declare a value is not allowed",
+            );
+          }
+        }
       }
     }
   }
@@ -80,12 +84,13 @@ mod tests {
   fn no_this_alias_valid() {
     assert_lint_ok::<NoThisAlias>("const self = foo(this);");
     assert_lint_ok::<NoThisAlias>("const self = 'this';");
+    assert_lint_ok::<NoThisAlias>("const { props, state } = this;");
+    assert_lint_ok::<NoThisAlias>("const [foo] = this;");
   }
 
   #[test]
   fn no_this_alias_invalid() {
     assert_lint_err::<NoThisAlias>("const self = this;", 0);
-    assert_lint_err::<NoThisAlias>("const { props, state } = this;", 0);
     assert_lint_err_on_line_n::<NoThisAlias>(
       "
 var unscoped = this;
@@ -113,13 +118,9 @@ class TestClass {
       
   public act(scope: this = this) {
     const inMemberFunction = this;
-    const { act } = this;
-    const { act, constructor } = this;
-    const [foo] = this;
-    const [foo, bar] = this;
   }
 }",
-      vec![(4, 4), (5, 4), (13, 4), (14, 4), (15, 4), (16, 4), (17, 4)],
+      vec![(4, 4), (5, 4), (13, 4)],
     );
   }
 }
