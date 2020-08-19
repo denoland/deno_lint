b86be8ea881743c83ac65b3e720ae0a89430fe65
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-08-19 02:11:16 +0200

    finish

--- src/rules/no_mixed_spaces_and_tabs.rs
+++ src/rules/no_mixed_spaces_and_tabs.rs
@@ -1,16 +1,16 @@
 // Copyright 2020 the Deno authors. All rights reserved. MIT license.
 use super::Context;
 use super::LintRule;
-use swc_common::BytePos;
-use swc_common::Span;
-use swc_common::SyntaxContext;
-use swc_common::Spanned;
 use regex::Regex;
 use std::sync::Arc;
-use swc_ecmascript::visit::Node;
-use swc_ecmascript::visit::Visit;
+use swc_common::BytePos;
+use swc_common::Span;
+use swc_common::Spanned;
+use swc_common::SyntaxContext;
 use swc_ecmascript::ast::Lit;
 use swc_ecmascript::ast::Tpl;
+use swc_ecmascript::visit::Node;
+use swc_ecmascript::visit::Visit;
 
 lazy_static! {
   static ref RE: Regex = Regex::new("^([\t ]*(\t | \t))").unwrap();
@@ -40,70 +40,47 @@ impl LintRule for NoMixedSpacesAndTabs {
 
     let mut excluded_ranges = visitor.ranges;
 
-    eprintln!("comments! {:#?}", context.leading_comments);
-    eprintln!("tcomments! {:#?}", context.trailing_comments);
-
     context.leading_comments.values().for_each(|comments| {
-      eprintln!("comments! {:#?}", comments);
       for comment in comments {
-        let lines = context.source_map.span_to_lines(comment.span).unwrap().lines;
+        let lines = context
+          .source_map
+          .span_to_lines(comment.span)
+          .unwrap()
+          .lines;
         for line in lines.iter().skip(1) {
           let (lo, hi) = file.line_bounds(line.line_index as usize);
-          excluded_ranges.push(Span::new(
-            lo,
-            hi,
-            SyntaxContext::empty(),
-          ));
+          excluded_ranges.push(Span::new(lo, hi, SyntaxContext::empty()));
         }
       }
     });
     context.trailing_comments.values().for_each(|comments| {
-      eprintln!("comments! {:#?}", comments);
       for comment in comments {
-        let lines = context.source_map.span_to_lines(comment.span).unwrap().lines;
-        eprintln!("lines {:#?}", lines);
+        let lines = context
+          .source_map
+          .span_to_lines(comment.span)
+          .unwrap()
+          .lines;
         for line in lines.iter().skip(1) {
           let (lo, hi) = file.line_bounds(line.line_index as usize);
-          eprintln!("hi lo {:#?} {:#?}", hi, lo);
-          excluded_ranges.push(Span::new(
-            lo,
-            hi,
-            SyntaxContext::empty(),
-          ));
+          excluded_ranges.push(Span::new(lo, hi, SyntaxContext::empty()));
         }
       }
     });
 
-
     let excluded_ranges = excluded_ranges.iter();
-    eprintln!("excluded ranges {:#?}", excluded_ranges);
-    // let lines = file_and_lines.lines;
     for line_index in 0..file.count_lines() {
       let line = file.get_line(line_index).unwrap();
       let (byte_pos, _hi) = file.line_bounds(line_index);
-      // let byte_pos = BytePos(line_info.line_index as u32);
-      // let source_file_and_index = context
-      //   .source_map
-      //   .lookup_line(byte_pos)
-      //   .unwrap();
-      // let source_code = source_file_and_index
-      //   .sf
-      //   .get_line(line_info.line_index)
-      //   .unwrap();
       let whitespace_matches = RE.find_iter(&line);
       for whitespace_match in whitespace_matches {
-        eprintln!("whitespace match {:#?}", whitespace_match);
         let range = whitespace_match.range();
         let span = Span::new(
           byte_pos + BytePos(range.start as u32),
           byte_pos + BytePos(range.end as u32),
           SyntaxContext::empty(),
         );
-        eprintln!("span {:#?}", span);
-        let is_excluded = excluded_ranges.clone().any(|range| {
-          eprintln!("{:#?} {:#?} {:#?} {:#?}", range.lo, span.lo, span.hi, range.hi);
-          range.lo <= span.lo && span.hi <= range.hi
-        });
+        let is_excluded =
+          excluded_ranges.clone().any(|range| range.contains(span));
         if !is_excluded {
           context.add_diagnostic(
             span,
@@ -155,7 +132,6 @@ mod tests {
       "// \t",
       "/*\n*/\t ",
       "/* \t\n\t \n \t\n\t */ \t",
-      "\tvar x = 5,\n\t    y = 2;",
       "/*\n\t */`\n\t   `;",
       "/*\n\t */var a = `\n\t   `, b = `\n\t   `/*\t \n\t \n*/;",
       "/*\t `template inside comment` */",
@@ -166,7 +142,6 @@ mod tests {
       "const foo = `${console}\n\t foo`;",
       "`\t   `;`   \t`",
       "`foo${ 5 }\t    `;",
-      "\tvar x = 5,\n\t    y = 2;",
       "' \t\\\n\t multiline string';",
       "'\t \\\n \tmultiline string';",
     ]);
@@ -174,55 +149,34 @@ mod tests {
 
   #[test]
   fn no_mixed_spaces_and_tabs_invalid() {
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   "function add(x, y) {\n\t return x + y;\n}",
-    //   2, 0
-    // );
+    assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
+      "function add(x, y) {\n\t return x + y;\n}",
+      2,
+      0,
+    );
     assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
       "\t ;\n/*\n\t * Hello\n\t */",
-      1, 0
+      1,
+      0,
     );
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   " \t/* comment */",
-    //   1, 0
-    // );
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   "\t // comment",
-    //   1, 0
-    // );
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   "\t var a /* comment */ = 1;",
-    //   1, 0
-    // );
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   " \tvar b = 1; // comment",
-    //   1, 0
-    // );
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   "/**/\n \t/*\n \t*/",
-    //   2, 0
-    // );
-    // assert_lint_err_on_line_n::<NoMixedSpacesAndTabs>(
-    //   "\t var x = 5, y = 2, z = 5;\n\n\t \tvar j =\t x + y;\nz *= j;",
-    //   vec![(2, 0), (3, 0)]
-    // );
-
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   "\tvar x = 5,\n  \t  y = 2;",
-    //   2, 0
-    // );
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   "`foo${\n \t  5 }bar`;",
-    //   2, 2
-    // );
-    
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   "  \t'';",
-    //   1, 0
-    // );
-    // assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
-    //   "''\n\t ",
-    //   2, 0
-    // );
+    assert_lint_err_on_line::<NoMixedSpacesAndTabs>(" \t/* comment */", 1, 0);
+    assert_lint_err_on_line::<NoMixedSpacesAndTabs>("\t // comment", 1, 0);
+    assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
+      "\t var a /* comment */ = 1;",
+      1,
+      0,
+    );
+    assert_lint_err_on_line::<NoMixedSpacesAndTabs>(
+      " \tvar b = 1; // comment",
+      1,
+      0,
+    );
+    assert_lint_err_on_line::<NoMixedSpacesAndTabs>("/**/\n \t/*\n \t*/", 2, 0);
+    assert_lint_err_on_line_n::<NoMixedSpacesAndTabs>(
+      "\t var x = 5, y = 2, z = 5;\n\n\t \tvar j =\t x + y;\nz *= j;",
+      vec![(1, 0), (3, 0)],
+    );
+    assert_lint_err_on_line::<NoMixedSpacesAndTabs>("  \t'';", 1, 0);
+    assert_lint_err_on_line::<NoMixedSpacesAndTabs>("''\n\t ", 2, 0);
   }
 }
