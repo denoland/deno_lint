da04af780332260e987dce56597cb4a22e310a87
강동윤 <kdy1997.dev@gmail.com>
2020-08-26 15:30:39 +0900

    Basical design is done

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -1,6 +1,41 @@
-use swc_ecmascript::ast::{BlockStmt, Stmt};
+use std::collections::HashMap;
+use swc_common::BytePos;
+use swc_ecmascript::ast::*;
+use swc_ecmascript::visit::{Node, Visit};
 
-pub trait EndsWithRet {
+pub struct Info {
+  stmts: HashMap<BytePos, StmtInfo>,
+}
+
+impl Info {
+  pub fn stmt(&self, lo: BytePos) -> Option<&StmtInfo> {
+    self.stmts.get(&lo)
+  }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BlockKind {}
+
+#[derive(Debug, Clone)]
+pub struct StmtInfo {
+  path: Vec<BlockKind>,
+}
+
+impl StmtInfo {
+  pub fn path(&self) -> &[BlockKind] {
+    &self.path
+  }
+}
+
+struct Scope<'a> {
+  parent: Option<&'a Scope<'a>>,
+}
+
+impl Visit for Info {
+  fn visit_fn_decl(&mut self, n: &FnDecl, _: &dyn Node) {}
+}
+
+pub(crate) trait EndsWithRet {
   /// Does a node ends with return, throw, break, continue?
   fn ends_with_ret(&self) -> bool;
 }
@@ -15,7 +50,21 @@ impl EndsWithRet for Stmt {
       Stmt::Block(s) => s.ends_with_ret(),
       Stmt::With(s) => s.body.ends_with_ret(),
       Stmt::Labeled(s) => s.body.ends_with_ret(),
-
+      Stmt::If(s) => s.cons.ends_with_ret() && s.alt.ends_with_ret(),
+      Stmt::Switch(s) => s.cases.iter().all(|case| case.cons.ends_with_ret()),
+      Stmt::Try(s) => match s {
+        TryStmt {
+          finalizer: None, ..
+        } => s.block.ends_with_ret(),
+        // TODO: Improve to eslint grade
+        _ => s.finalizer.ends_with_ret(),
+      },
+      // Stmt::While(_) => {}
+      // Stmt::DoWhile(_) => {}
+      // Stmt::For(_) => {}
+      // Stmt::ForIn(_) => {}
+      // Stmt::ForOf(_) => {}
+      Stmt::Decl(_) | Stmt::Expr(_) => false,
       _ => false,
     }
   }
@@ -30,5 +79,23 @@ impl EndsWithRet for BlockStmt {
 impl EndsWithRet for Vec<Stmt> {
   fn ends_with_ret(&self) -> bool {
     self.last().map(|s| s.ends_with_ret()).unwrap_or(false)
+  }
+}
+
+impl<T> EndsWithRet for Option<T>
+where
+  T: EndsWithRet,
+{
+  fn ends_with_ret(&self) -> bool {
+    self.as_ref().map(|s| s.ends_with_ret()).unwrap_or(false)
+  }
+}
+
+impl<T> EndsWithRet for Box<T>
+where
+  T: EndsWithRet,
+{
+  fn ends_with_ret(&self) -> bool {
+    (**self).ends_with_ret()
   }
 }
