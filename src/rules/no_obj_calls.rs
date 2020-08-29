64090940b7df07a4c55beef9ea7206c1a24ea651
Yusuke Tanaka <yusuktan@maguro.dev>
2020-08-29 19:57:08 +0900

    Chore: sort rules alphabetically (#289)

--- src/rules/no_obj_calls.rs
+++ src/rules/no_obj_calls.rs
@@ -20,7 +20,7 @@ impl LintRule for NoObjCalls {
   }
 
   fn code(&self) -> &'static str {
-    "no-obj-call"
+    "no-obj-calls"
   }
 
   fn lint_module(
@@ -48,7 +48,7 @@ impl NoObjCallsVisitor {
       "Math" | "JSON" | "Reflect" | "Atomics" => {
         self.context.add_diagnostic(
           span,
-          "no-obj-call",
+          "no-obj-calls",
           format!("`{}` call as function is not allowed", callee_name).as_ref(),
         );
       }
