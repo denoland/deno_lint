1f3c631686e7e622bc8051b9c55191878832b2e0
강동윤 <kdy1997.dev@gmail.com>
2020-09-08 21:34:07 +0900

    Add ok tests

--- src/rules/no_fallthrough.rs
+++ src/rules/no_fallthrough.rs
@@ -79,180 +79,140 @@ mod tests {
   #[test]
   fn ok_1() {
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: a(); /* falls through */ case 1: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: a()\n /* falls through */ case 1: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: a(); /* fall through */ case 1: b(); }",
     );
   }
 
   #[test]
   fn ok_2() {
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: a(); /* fallthrough */ case 1: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: a(); /* FALLS THROUGH */ case 1: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "function foo() { switch(foo) { case 0: a(); return; case 1: b(); }; }",
     );
   }
 
   #[test]
   fn ok_3() {
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: a(); throw 'foo'; case 1: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "while (a) { switch(foo) { case 0: a(); continue; case 1: b(); } }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: a(); break; case 1: b(); }",
     );
   }
 
   #[test]
   fn ok_4() {
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: case 1: a(); break; case 2: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: case 1: break; case 2: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: case 1: break; default: b(); }",
     );
   }
 
   #[test]
   fn ok_5() {
-    assert_lint_ok::<NoFallthrough>(
-      "
-      ",
-    );
+    assert_lint_ok::<NoFallthrough>("switch(foo) { case 0: case 1: a(); }");
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: case 1: a(); break; }",
     );
 
-    assert_lint_ok::<NoFallthrough>(
-      "
-      ",
-    );
+    assert_lint_ok::<NoFallthrough>("switch(foo) { case 0: case 1: break; }");
   }
 
   #[test]
   fn ok_6() {
+    assert_lint_ok::<NoFallthrough>("switch(foo) { case 0:\n case 1: break; }");
+
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: // comment\n case 1: break; }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
-    );
-
-    assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "function foo() { switch(foo) { case 0: case 1: return; } }",
     );
   }
 
   #[test]
   fn ok_7() {
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "function foo() { switch(foo) { case 0: {return;}\n case 1: {return;} } }",
     );
 
-    assert_lint_ok::<NoFallthrough>(
-      "
-      ",
-    );
+    assert_lint_ok::<NoFallthrough>("switch(foo) { case 0: case 1: {break;} }");
 
-    assert_lint_ok::<NoFallthrough>(
-      "
-      ",
-    );
+    assert_lint_ok::<NoFallthrough>("switch(foo) { }");
   }
 
   #[test]
   fn ok_8() {
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch(foo) { case 0: switch(bar) { case 2: break; } /* falls through */ case 1: break; }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "function foo() { switch(foo) { case 1: return a; a++; }}",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch (foo) { case 0: a(); /* falls through */ default:  b(); /* comment */ }",
     );
   }
 
   #[test]
   fn ok_9() {
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch (foo) { case 0: a(); /* falls through */ default: /* comment */ b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch (foo) { case 0: if (a) { break; } else { throw 0; } default: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch (foo) { case 0: try { break; } finally {} default: b(); }",
     );
   }
 
   #[test]
   fn ok_10() {
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch (foo) { case 0: try {} finally { break; } default: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch (foo) { case 0: try { throw 0; } catch (err) { break; } default: b(); }",
     );
 
     assert_lint_ok::<NoFallthrough>(
-      "
-      ",
+      "switch (foo) { case 0: do { throw 0; } while(a); default: b(); }",
     );
   }
 }
