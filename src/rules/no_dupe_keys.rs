1cb0a224e5d1638240ccfbe1ad422856be4c5e41
Dom Parfitt <mail@domparfitt.com>
2020-10-07 13:52:43 +0100

    Adds docs to no dupe keys rule

--- src/rules/no_dupe_keys.rs
+++ src/rules/no_dupe_keys.rs
@@ -23,6 +23,39 @@ impl LintRule for NoDupeKeys {
     let mut visitor = NoDupeKeysVisitor::new(context);
     visitor.visit_module(module, module);
   }
+
+  fn docs(&self) -> &'static str {
+    r#"Disallows duplicate keys in object literals.
+
+Setting the same key multiple times in an object literal will override other assignments to that key and can cause unexpected behaviour.
+
+### Invalid:
+```typescript
+const foo = {
+  bar: "baz",
+  bar: "qux"
+};
+```
+```typescript
+const foo = {
+  "bar": "baz",
+  bar: "qux"
+};
+```
+```typescript
+const foo = {
+  0x1: "baz",
+  1: "qux"
+};
+```
+### Valid:
+```typescript
+var foo = {
+  bar: "baz",
+  quxx: "qux"
+};
+```"#
+  }
 }
 
 struct NoDupeKeysVisitor {
