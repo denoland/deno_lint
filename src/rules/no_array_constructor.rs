3900ae49def36c5ba92c782c4c636e727e3476b2
Chris Knight <cknight1234@gmail.com>
2020-10-21 12:25:10 +0100

    doc: add docs (#427)

--- src/rules/no_array_constructor.rs
+++ src/rules/no_array_constructor.rs
@@ -31,6 +31,36 @@ impl LintRule for NoArrayConstructor {
     let mut visitor = NoArrayConstructorVisitor::new(context);
     module.visit_all_with(module, &mut visitor);
   }
+
+  fn docs(&self) -> &'static str {
+    r#"Enforce conventional usage of array construction
+
+Array construction is conventionally done via literal notation such as `[]` or
+`[1,2,3]`.  Using the `new Array()` is discouraged as is `new Array(1,2,3)`. There
+are two reasons for this.  The first is that a single supplied argument defines
+the array length, while multiple arguments instead populate the array of no fixed
+size.  This confusion is avoided when pre-populated arrays are only created using
+literal notation.  The second argument to avoiding the `Array` constructor is that
+the `Array` global may be redefined.
+
+The one exception to this rule is when creating a new array of fixed size, e.g.
+`new Array(6)`.  This is the conventional way to create arrays of fixed length.
+    
+### Valid:
+```typescript
+const a = new Array(100);
+const b = [];
+const c = [1,2,3];
+```
+
+### Invalid:
+```typescript
+// This is 4 elements, not a size 100 array of 3 elements
+const a = new Array(100, 1, 2, 3);
+
+const b = new Array(); // use [] instead
+```"#
+  }
 }
 
 struct NoArrayConstructorVisitor<'c> {
@@ -44,10 +74,11 @@ impl<'c> NoArrayConstructorVisitor<'c> {
 
   fn check_args(&mut self, args: Vec<ExprOrSpread>, span: Span) {
     if args.len() != 1 {
-      self.context.add_diagnostic(
+      self.context.add_diagnostic_with_hint(
         span,
         "no-array-constructor",
         "Array Constructor is not allowed",
+        "Use array literal notation (e.g. []) or single argument specifying array size only (e.g. new Array(5)",
       );
     }
   }
--- src/rules/no_async_promise_executor.rs
+++ src/rules/no_async_promise_executor.rs
@@ -30,6 +30,35 @@ impl LintRule for NoAsyncPromiseExecutor {
     let mut visitor = NoAsyncPromiseExecutorVisitor::new(context);
     module.visit_all_with(module, &mut visitor);
   }
+
+  fn docs(&self) -> &'static str {
+    r#"Requires that async promise executor functions are not used
+
+Promise constructors take an executor function as an argument with `resolve` and 
+`reject` parameters that can be used to control the state of the created Promise.
+This function is allowed to be async but this is generally not a good idea for
+several reasons:
+* If an async executor function throws an error, the error will be lost and won't
+cause the newly-constructed Promise to reject. This could make it difficult to
+debug and handle some errors.
+* If an async Promise executor function is using await, then this is usually a
+sign that it is not actually necessary to use the new Promise constructor and the
+code can be restructured to avoid the use of a promise, or the scope of the new
+Promise constructor can be reduced, extracting the async code and changing it to
+be synchronous.
+    
+### Valid:
+```typescript
+new Promise(function(resolve, reject) {});
+new Promise((resolve, reject) => {});
+```
+
+### Invalid:
+```typescript
+new Promise(async function(resolve, reject) {});
+new Promise(async (resolve, reject) => {});
+```"#
+  }
 }
 
 struct NoAsyncPromiseExecutorVisitor<'c> {
@@ -64,10 +93,11 @@ impl<'c> VisitAll for NoAsyncPromiseExecutorVisitor<'c> {
       if let Some(args) = &new_expr.args {
         if let Some(first_arg) = args.get(0) {
           if is_async_function(&*first_arg.expr) {
-            self.context.add_diagnostic(
+            self.context.add_diagnostic_with_hint(
               new_expr.span,
               "no-async-promise-executor",
               "Async promise executors are not allowed",
+              "Remove `async` from executor function and adjust promise code as needed",
             );
           }
         }
