2010e83beece8011e8de3b4fa873b02d017f39d8
Yusuke Tanaka <yusuktan@maguro.dev>
2020-10-25 06:51:17 +0900

    refactor(ban-untagged-ignore): switch to `assert_lint_err!` macro (#436)

--- src/rules/ban_untagged_ignore.rs
+++ src/rules/ban_untagged_ignore.rs
@@ -69,7 +69,6 @@ export function duplicateArgumentsFn(a, b, a) { }
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::test_util::*;
 
   #[test]
   fn ban_ts_ignore_valid() {
@@ -86,15 +85,21 @@ function bar() {
 
   #[test]
   fn ban_ts_ignore_invalid() {
-    assert_lint_err_on_line::<BanUntaggedIgnore>(
+    assert_lint_err! {
+      BanUntaggedIgnore,
       r#"
 // deno-lint-ignore
 function foo() {
   // pass
 }
-    "#,
-      2,
-      0,
-    );
+      "#: [
+        {
+          line: 2,
+          col: 0,
+          message: "Ignore directive requires lint rule name(s)",
+          hint: "Add one or more lint rule names.  E.g. // deno-lint-ignore adjacent-overload-signatures",
+        }
+      ]
+    };
   }
 }
