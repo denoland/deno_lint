ea9f3e74e545e03d90c456f19ab1a85d0b09e7c0
Luca Casonato <lucacasonato@yahoo.com>
2020-12-09 11:46:35 +0100

    update dext

--- www/deps.ts
+++ www/deps.ts
@@ -1,10 +1,10 @@
-export { Fragment, h } from "https://x.lcas.dev/preact@10.5.3/mod.js";
+export { Fragment, h } from "https://x.lcas.dev/preact@10.5.7/mod.js";
 export {
   useEffect,
   useMemo,
   useRef,
   useState,
-} from "https://x.lcas.dev/preact@10.5.3/hooks.js";
+} from "https://x.lcas.dev/preact@10.5.7/hooks.js";
 export type {
   AppProps,
   DocumentProps,
@@ -12,6 +12,6 @@ export type {
   GetStaticDataContext,
   GetStaticPaths,
   PageProps,
-} from "https://deno.land/x/dext@0.8.0/mod.ts";
+} from "https://deno.land/x/dext@0.9.5/mod.ts";
 import MarkdownIt from "https://dev.jspm.io/markdown-it@11.0.0";
 export { MarkdownIt };
