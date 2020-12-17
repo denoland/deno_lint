8c4141ed2c8ae2f153b90a691c98ec6882236fdc
Yusuke Tanaka <yusuktan@maguro.dev>
2020-12-18 00:59:49 +0900

    add meta tags for open graph

--- www/pages/_document.tsx
+++ www/pages/_document.tsx
@@ -8,6 +8,16 @@ function Document(props: DocumentProps) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>deno_lint</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="deno_lint" />
+        <meta property="og:url" content="https://lint.deno.land" />
+        <meta property="og:title" content="deno_lint docs" />
+        <meta
+          property="og:description"
+          content="deno_lint documentation for every lint rule"
+        />
+        <meta property="og:image" content="https://deno.land/images/hashrock_simple.png" />
+        <meta name="twitter:card" content="summary" />
         <link rel="stylesheet" href="/style/main.css" />
         <link
           rel="stylesheet"
