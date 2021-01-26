041ae03b46056f920f279826ef3bdf514ef4ade6
Yusuke Tanaka <yusuktan@maguro.dev>
2021-01-27 00:15:42 +0900

    run no_with using ast-view

--- src/rules/no_with.rs
+++ src/rules/no_with.rs
@@ -1,7 +1,9 @@
 // Copyright 2020-2021 the Deno authors. All rights reserved. MIT license.
 use super::Context;
 use super::LintRule;
-use swc_ecmascript::ast::WithStmt;
+use crate::handler::{handle_node, Handler};
+use dprint_swc_ecma_ast_view::{with_ast_view, SourceFileInfo};
+use swc_ecmascript::ast::{Program, WithStmt};
 use swc_ecmascript::visit::noop_visit_type;
 use swc_ecmascript::visit::Node;
 use swc_ecmascript::visit::Visit;
@@ -24,13 +26,31 @@ impl LintRule for NoWith {
     CODE
   }
 
-  fn lint_program(
-    &self,
-    context: &mut Context,
-    program: &swc_ecmascript::ast::Program,
-  ) {
+  fn lint_program(&self, context: &mut Context, program: &Program) {
     let mut visitor = NoWithVisitor::new(context);
     visitor.visit_program(program, program);
+  }
+
+  fn lint_program_with_ast_view(
+    &self,
+    context: &mut Context,
+    program: &Program,
+  ) {
+    if let Program::Module(module) = program {
+      let info = SourceFileInfo {
+        module,
+        source_file: None,
+        tokens: None,
+        comments: None,
+      };
+
+      with_ast_view(info, |module| {
+        let mut handler = NoWithVisitor::new(context);
+        handle_node(module, &mut handler);
+      });
+    } else {
+      self.lint_program(context, program);
+    }
   }
 }
 
@@ -52,6 +72,14 @@ impl<'c> Visit for NoWithVisitor<'c> {
   }
 }
 
+impl<'c> Handler for NoWithVisitor<'c> {
+  fn with_stmt(&mut self, with_stmt: &dprint_swc_ecma_ast_view::WithStmt) {
+    self
+      .context
+      .add_diagnostic(with_stmt.inner.span, CODE, MESSAGE);
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -61,6 +89,11 @@ mod tests {
     assert_lint_err! {
       NoWith,
       "with (someVar) { console.log('asdf'); }": [{ col: 0, message: MESSAGE }],
+
+      // This is the same as the above one in the sense that what is tested,
+      // but it's present only for with_ast_view testing, which requires that the code is parsed as
+      // Module, not as Script.
+      "with (someVar) { console.log('asdf'); } export const foo = 42;": [{ col: 0, message: MESSAGE }],
     }
   }
 }
