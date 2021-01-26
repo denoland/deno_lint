d7acbcd564f9ad515774f7b7309d1808627caa2c
Yusuke Tanaka <yusuktan@maguro.dev>
2021-01-26 23:38:25 +0900

    fix handler

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,6 @@
 use dprint_swc_ecma_ast_view::{self as AstView, NodeKind, NodeTrait};
 
-pub fn handle_node<'a, N, H>(node: &N, handler: &mut H)
+pub fn handle_node<'a, N, H>(node: N, handler: &mut H)
 where
   N: NodeTrait<'a>,
   H: Handler,
@@ -646,182 +646,184 @@ where
     }
   }
 
-  for child in &node.children() {
+  for child in node.children() {
     handle_node(child, handler);
   }
 }
 
 pub trait Handler {
-  fn array_lit(&mut self, n: &AstView::ArrayLit) {}
-  fn array_pat(&mut self, n: &AstView::ArrayPat) {}
-  fn arrow_expr(&mut self, n: &AstView::ArrowExpr) {}
-  fn assign_expr(&mut self, n: &AstView::AssignExpr) {}
-  fn assign_pat(&mut self, n: &AstView::AssignPat) {}
-  fn assign_pat_prop(&mut self, n: &AstView::AssignPatProp) {}
-  fn assign_prop(&mut self, n: &AstView::AssignProp) {}
-  fn await_expr(&mut self, n: &AstView::AwaitExpr) {}
-  fn big_int(&mut self, n: &AstView::BigInt) {}
-  fn bin_expr(&mut self, n: &AstView::BinExpr) {}
-  fn block_stmt(&mut self, n: &AstView::BlockStmt) {}
-  fn bool(&mut self, n: &AstView::Bool) {}
-  fn break_stmt(&mut self, n: &AstView::BreakStmt) {}
-  fn call_expr(&mut self, n: &AstView::CallExpr) {}
-  fn catch_clause(&mut self, n: &AstView::CatchClause) {}
-  fn class(&mut self, n: &AstView::Class) {}
-  fn class_decl(&mut self, n: &AstView::ClassDecl) {}
-  fn class_expr(&mut self, n: &AstView::ClassExpr) {}
-  fn class_method(&mut self, n: &AstView::ClassMethod) {}
-  fn class_prop(&mut self, n: &AstView::ClassProp) {}
-  fn computed_prop_name(&mut self, n: &AstView::ComputedPropName) {}
-  fn cond_expr(&mut self, n: &AstView::CondExpr) {}
-  fn constructor(&mut self, n: &AstView::Constructor) {}
-  fn continue_stmt(&mut self, n: &AstView::ContinueStmt) {}
-  fn debugger_stmt(&mut self, n: &AstView::DebuggerStmt) {}
-  fn decorator(&mut self, n: &AstView::Decorator) {}
-  fn do_while_stmt(&mut self, n: &AstView::DoWhileStmt) {}
-  fn empty_stmt(&mut self, n: &AstView::EmptyStmt) {}
-  fn export_all(&mut self, n: &AstView::ExportAll) {}
-  fn export_decl(&mut self, n: &AstView::ExportDecl) {}
-  fn export_default_decl(&mut self, n: &AstView::ExportDefaultDecl) {}
-  fn export_default_expr(&mut self, n: &AstView::ExportDefaultExpr) {}
-  fn export_default_specifier(&mut self, n: &AstView::ExportDefaultSpecifier) {}
-  fn export_named_specifier(&mut self, n: &AstView::ExportNamedSpecifier) {}
+  fn array_lit(&mut self, _n: &AstView::ArrayLit) {}
+  fn array_pat(&mut self, _n: &AstView::ArrayPat) {}
+  fn arrow_expr(&mut self, _n: &AstView::ArrowExpr) {}
+  fn assign_expr(&mut self, _n: &AstView::AssignExpr) {}
+  fn assign_pat(&mut self, _n: &AstView::AssignPat) {}
+  fn assign_pat_prop(&mut self, _n: &AstView::AssignPatProp) {}
+  fn assign_prop(&mut self, _n: &AstView::AssignProp) {}
+  fn await_expr(&mut self, _n: &AstView::AwaitExpr) {}
+  fn big_int(&mut self, _n: &AstView::BigInt) {}
+  fn bin_expr(&mut self, _n: &AstView::BinExpr) {}
+  fn block_stmt(&mut self, _n: &AstView::BlockStmt) {}
+  fn bool(&mut self, _n: &AstView::Bool) {}
+  fn break_stmt(&mut self, _n: &AstView::BreakStmt) {}
+  fn call_expr(&mut self, _n: &AstView::CallExpr) {}
+  fn catch_clause(&mut self, _n: &AstView::CatchClause) {}
+  fn class(&mut self, _n: &AstView::Class) {}
+  fn class_decl(&mut self, _n: &AstView::ClassDecl) {}
+  fn class_expr(&mut self, _n: &AstView::ClassExpr) {}
+  fn class_method(&mut self, _n: &AstView::ClassMethod) {}
+  fn class_prop(&mut self, _n: &AstView::ClassProp) {}
+  fn computed_prop_name(&mut self, _n: &AstView::ComputedPropName) {}
+  fn cond_expr(&mut self, _n: &AstView::CondExpr) {}
+  fn constructor(&mut self, _n: &AstView::Constructor) {}
+  fn continue_stmt(&mut self, _n: &AstView::ContinueStmt) {}
+  fn debugger_stmt(&mut self, _n: &AstView::DebuggerStmt) {}
+  fn decorator(&mut self, _n: &AstView::Decorator) {}
+  fn do_while_stmt(&mut self, _n: &AstView::DoWhileStmt) {}
+  fn empty_stmt(&mut self, _n: &AstView::EmptyStmt) {}
+  fn export_all(&mut self, _n: &AstView::ExportAll) {}
+  fn export_decl(&mut self, _n: &AstView::ExportDecl) {}
+  fn export_default_decl(&mut self, _n: &AstView::ExportDefaultDecl) {}
+  fn export_default_expr(&mut self, _n: &AstView::ExportDefaultExpr) {}
+  fn export_default_specifier(&mut self, _n: &AstView::ExportDefaultSpecifier) {
+  }
+  fn export_named_specifier(&mut self, _n: &AstView::ExportNamedSpecifier) {}
   fn export_namespace_specifier(
     &mut self,
-    n: &AstView::ExportNamespaceSpecifier,
+    _n: &AstView::ExportNamespaceSpecifier,
   ) {
   }
-  fn expr_or_spread(&mut self, n: &AstView::ExprOrSpread) {}
-  fn expr_stmt(&mut self, n: &AstView::ExprStmt) {}
-  fn fn_decl(&mut self, n: &AstView::FnDecl) {}
-  fn fn_expr(&mut self, n: &AstView::FnExpr) {}
-  fn for_in_stmt(&mut self, n: &AstView::ForInStmt) {}
-  fn for_of_stmt(&mut self, n: &AstView::ForOfStmt) {}
-  fn for_stmt(&mut self, n: &AstView::ForStmt) {}
-  fn function(&mut self, n: &AstView::Function) {}
-  fn getter_prop(&mut self, n: &AstView::GetterProp) {}
-  fn ident(&mut self, n: &AstView::Ident) {}
-  fn if_stmt(&mut self, n: &AstView::IfStmt) {}
-  fn import_decl(&mut self, n: &AstView::ImportDecl) {}
-  fn import_default_specifier(&mut self, n: &AstView::ImportDefaultSpecifier) {}
-  fn import_named_specifier(&mut self, n: &AstView::ImportNamedSpecifier) {}
-  fn import_star_as_specifier(&mut self, n: &AstView::ImportStarAsSpecifier) {}
-  fn invalid(&mut self, n: &AstView::Invalid) {}
-  fn jsx_attr(&mut self, n: &AstView::JSXAttr) {}
-  fn jsx_closing_element(&mut self, n: &AstView::JSXClosingElement) {}
-  fn jsx_closing_fragment(&mut self, n: &AstView::JSXClosingFragment) {}
-  fn jsx_element(&mut self, n: &AstView::JSXElement) {}
-  fn jsx_empty_expr(&mut self, n: &AstView::JSXEmptyExpr) {}
-  fn jsx_expr_container(&mut self, n: &AstView::JSXExprContainer) {}
-  fn jsx_fragment(&mut self, n: &AstView::JSXFragment) {}
-  fn jsx_member_expr(&mut self, n: &AstView::JSXMemberExpr) {}
-  fn jsx_namespaced_name(&mut self, n: &AstView::JSXNamespacedName) {}
-  fn jsx_opening_element(&mut self, n: &AstView::JSXOpeningElement) {}
-  fn jsx_opening_fragment(&mut self, n: &AstView::JSXOpeningFragment) {}
-  fn jsx_spread_child(&mut self, n: &AstView::JSXSpreadChild) {}
-  fn jsx_text(&mut self, n: &AstView::JSXText) {}
-  fn key_value_pat_prop(&mut self, n: &AstView::KeyValuePatProp) {}
-  fn key_value_prop(&mut self, n: &AstView::KeyValueProp) {}
-  fn labeled_stmt(&mut self, n: &AstView::LabeledStmt) {}
-  fn member_expr(&mut self, n: &AstView::MemberExpr) {}
-  fn meta_prop_expr(&mut self, n: &AstView::MetaPropExpr) {}
-  fn method_prop(&mut self, n: &AstView::MethodProp) {}
-  fn module(&mut self, n: &AstView::Module) {}
-  fn named_export(&mut self, n: &AstView::NamedExport) {}
-  fn new_expr(&mut self, n: &AstView::NewExpr) {}
-  fn null(&mut self, n: &AstView::Null) {}
-  fn number(&mut self, n: &AstView::Number) {}
-  fn object_lit(&mut self, n: &AstView::ObjectLit) {}
-  fn object_pat(&mut self, n: &AstView::ObjectPat) {}
-  fn opt_chain_expr(&mut self, n: &AstView::OptChainExpr) {}
-  fn param(&mut self, n: &AstView::Param) {}
-  fn paren_expr(&mut self, n: &AstView::ParenExpr) {}
-  fn private_method(&mut self, n: &AstView::PrivateMethod) {}
-  fn private_name(&mut self, n: &AstView::PrivateName) {}
-  fn private_prop(&mut self, n: &AstView::PrivateProp) {}
-  fn regex(&mut self, n: &AstView::Regex) {}
-  fn rest_pat(&mut self, n: &AstView::RestPat) {}
-  fn return_stmt(&mut self, n: &AstView::ReturnStmt) {}
-  fn seq_expr(&mut self, n: &AstView::SeqExpr) {}
-  fn setter_prop(&mut self, n: &AstView::SetterProp) {}
-  fn spread_element(&mut self, n: &AstView::SpreadElement) {}
-  fn str(&mut self, n: &AstView::Str) {}
+  fn expr_or_spread(&mut self, _n: &AstView::ExprOrSpread) {}
+  fn expr_stmt(&mut self, _n: &AstView::ExprStmt) {}
+  fn fn_decl(&mut self, _n: &AstView::FnDecl) {}
+  fn fn_expr(&mut self, _n: &AstView::FnExpr) {}
+  fn for_in_stmt(&mut self, _n: &AstView::ForInStmt) {}
+  fn for_of_stmt(&mut self, _n: &AstView::ForOfStmt) {}
+  fn for_stmt(&mut self, _n: &AstView::ForStmt) {}
+  fn function(&mut self, _n: &AstView::Function) {}
+  fn getter_prop(&mut self, _n: &AstView::GetterProp) {}
+  fn ident(&mut self, _n: &AstView::Ident) {}
+  fn if_stmt(&mut self, _n: &AstView::IfStmt) {}
+  fn import_decl(&mut self, _n: &AstView::ImportDecl) {}
+  fn import_default_specifier(&mut self, _n: &AstView::ImportDefaultSpecifier) {
+  }
+  fn import_named_specifier(&mut self, _n: &AstView::ImportNamedSpecifier) {}
+  fn import_star_as_specifier(&mut self, _n: &AstView::ImportStarAsSpecifier) {}
+  fn invalid(&mut self, _n: &AstView::Invalid) {}
+  fn jsx_attr(&mut self, _n: &AstView::JSXAttr) {}
+  fn jsx_closing_element(&mut self, _n: &AstView::JSXClosingElement) {}
+  fn jsx_closing_fragment(&mut self, _n: &AstView::JSXClosingFragment) {}
+  fn jsx_element(&mut self, _n: &AstView::JSXElement) {}
+  fn jsx_empty_expr(&mut self, _n: &AstView::JSXEmptyExpr) {}
+  fn jsx_expr_container(&mut self, _n: &AstView::JSXExprContainer) {}
+  fn jsx_fragment(&mut self, _n: &AstView::JSXFragment) {}
+  fn jsx_member_expr(&mut self, _n: &AstView::JSXMemberExpr) {}
+  fn jsx_namespaced_name(&mut self, _n: &AstView::JSXNamespacedName) {}
+  fn jsx_opening_element(&mut self, _n: &AstView::JSXOpeningElement) {}
+  fn jsx_opening_fragment(&mut self, _n: &AstView::JSXOpeningFragment) {}
+  fn jsx_spread_child(&mut self, _n: &AstView::JSXSpreadChild) {}
+  fn jsx_text(&mut self, _n: &AstView::JSXText) {}
+  fn key_value_pat_prop(&mut self, _n: &AstView::KeyValuePatProp) {}
+  fn key_value_prop(&mut self, _n: &AstView::KeyValueProp) {}
+  fn labeled_stmt(&mut self, _n: &AstView::LabeledStmt) {}
+  fn member_expr(&mut self, _n: &AstView::MemberExpr) {}
+  fn meta_prop_expr(&mut self, _n: &AstView::MetaPropExpr) {}
+  fn method_prop(&mut self, _n: &AstView::MethodProp) {}
+  fn module(&mut self, _n: &AstView::Module) {}
+  fn named_export(&mut self, _n: &AstView::NamedExport) {}
+  fn new_expr(&mut self, _n: &AstView::NewExpr) {}
+  fn null(&mut self, _n: &AstView::Null) {}
+  fn number(&mut self, _n: &AstView::Number) {}
+  fn object_lit(&mut self, _n: &AstView::ObjectLit) {}
+  fn object_pat(&mut self, _n: &AstView::ObjectPat) {}
+  fn opt_chain_expr(&mut self, _n: &AstView::OptChainExpr) {}
+  fn param(&mut self, _n: &AstView::Param) {}
+  fn paren_expr(&mut self, _n: &AstView::ParenExpr) {}
+  fn private_method(&mut self, _n: &AstView::PrivateMethod) {}
+  fn private_name(&mut self, _n: &AstView::PrivateName) {}
+  fn private_prop(&mut self, _n: &AstView::PrivateProp) {}
+  fn regex(&mut self, _n: &AstView::Regex) {}
+  fn rest_pat(&mut self, _n: &AstView::RestPat) {}
+  fn return_stmt(&mut self, _n: &AstView::ReturnStmt) {}
+  fn seq_expr(&mut self, _n: &AstView::SeqExpr) {}
+  fn setter_prop(&mut self, _n: &AstView::SetterProp) {}
+  fn spread_element(&mut self, _n: &AstView::SpreadElement) {}
+  fn str(&mut self, _n: &AstView::Str) {}
   // Neither `super` or `r#super` can be used here, so we use `super_` reluctantly
-  fn super_(&mut self, n: &AstView::Super) {}
-  fn switch_case(&mut self, n: &AstView::SwitchCase) {}
-  fn switch_stmt(&mut self, n: &AstView::SwitchStmt) {}
-  fn tagged_tpl(&mut self, n: &AstView::TaggedTpl) {}
-  fn this_expr(&mut self, n: &AstView::ThisExpr) {}
-  fn throw_stmt(&mut self, n: &AstView::ThrowStmt) {}
-  fn tpl(&mut self, n: &AstView::Tpl) {}
-  fn tpl_element(&mut self, n: &AstView::TplElement) {}
-  fn try_stmt(&mut self, n: &AstView::TryStmt) {}
-  fn ts_array_type(&mut self, n: &AstView::TsArrayType) {}
-  fn ts_as_expr(&mut self, n: &AstView::TsAsExpr) {}
-  fn ts_call_signature_decl(&mut self, n: &AstView::TsCallSignatureDecl) {}
-  fn ts_conditional_type(&mut self, n: &AstView::TsConditionalType) {}
-  fn ts_const_assertion(&mut self, n: &AstView::TsConstAssertion) {}
+  fn super_(&mut self, _n: &AstView::Super) {}
+  fn switch_case(&mut self, _n: &AstView::SwitchCase) {}
+  fn switch_stmt(&mut self, _n: &AstView::SwitchStmt) {}
+  fn tagged_tpl(&mut self, _n: &AstView::TaggedTpl) {}
+  fn this_expr(&mut self, _n: &AstView::ThisExpr) {}
+  fn throw_stmt(&mut self, _n: &AstView::ThrowStmt) {}
+  fn tpl(&mut self, _n: &AstView::Tpl) {}
+  fn tpl_element(&mut self, _n: &AstView::TplElement) {}
+  fn try_stmt(&mut self, _n: &AstView::TryStmt) {}
+  fn ts_array_type(&mut self, _n: &AstView::TsArrayType) {}
+  fn ts_as_expr(&mut self, _n: &AstView::TsAsExpr) {}
+  fn ts_call_signature_decl(&mut self, _n: &AstView::TsCallSignatureDecl) {}
+  fn ts_conditional_type(&mut self, _n: &AstView::TsConditionalType) {}
+  fn ts_const_assertion(&mut self, _n: &AstView::TsConstAssertion) {}
   fn ts_construct_signature_decl(
     &mut self,
-    n: &AstView::TsConstructSignatureDecl,
+    _n: &AstView::TsConstructSignatureDecl,
   ) {
   }
-  fn ts_constructor_type(&mut self, n: &AstView::TsConstructorType) {}
-  fn ts_enum_decl(&mut self, n: &AstView::TsEnumDecl) {}
-  fn ts_enum_member(&mut self, n: &AstView::TsEnumMember) {}
-  fn ts_export_assignment(&mut self, n: &AstView::TsExportAssignment) {}
-  fn ts_expr_with_type_args(&mut self, n: &AstView::TsExprWithTypeArgs) {}
-  fn ts_external_module_ref(&mut self, n: &AstView::TsExternalModuleRef) {}
-  fn ts_fn_type(&mut self, n: &AstView::TsFnType) {}
-  fn ts_import_equal_decl(&mut self, n: &AstView::TsImportEqualsDecl) {}
-  fn ts_import_type(&mut self, n: &AstView::TsImportType) {}
-  fn ts_index_signature(&mut self, n: &AstView::TsIndexSignature) {}
-  fn ts_indexed_access_type(&mut self, n: &AstView::TsIndexedAccessType) {}
-  fn ts_infer_type(&mut self, n: &AstView::TsInferType) {}
-  fn ts_interface_body(&mut self, n: &AstView::TsInterfaceBody) {}
-  fn ts_interface_decl(&mut self, n: &AstView::TsInterfaceDecl) {}
-  fn ts_intersection_type(&mut self, n: &AstView::TsIntersectionType) {}
-  fn ts_keyword_type(&mut self, n: &AstView::TsKeywordType) {}
-  fn ts_lit_type(&mut self, n: &AstView::TsLitType) {}
-  fn ts_mapped_type(&mut self, n: &AstView::TsMappedType) {}
-  fn ts_method_signature(&mut self, n: &AstView::TsMethodSignature) {}
-  fn ts_module_block(&mut self, n: &AstView::TsModuleBlock) {}
-  fn ts_module_decl(&mut self, n: &AstView::TsModuleDecl) {}
-  fn ts_namespace_decl(&mut self, n: &AstView::TsNamespaceDecl) {}
-  fn ts_namespace_export_decl(&mut self, n: &AstView::TsNamespaceExportDecl) {}
-  fn ts_non_null_expr(&mut self, n: &AstView::TsNonNullExpr) {}
-  fn ts_optional_type(&mut self, n: &AstView::TsOptionalType) {}
-  fn ts_param_prop(&mut self, n: &AstView::TsParamProp) {}
-  fn ts_parenthesized_type(&mut self, n: &AstView::TsParenthesizedType) {}
-  fn ts_property_signature(&mut self, n: &AstView::TsPropertySignature) {}
-  fn ts_qualified_name(&mut self, n: &AstView::TsQualifiedName) {}
-  fn ts_rest_type(&mut self, n: &AstView::TsRestType) {}
-  fn ts_this_type(&mut self, n: &AstView::TsThisType) {}
-  fn ts_tpl_lit_type(&mut self, n: &AstView::TsTplLitType) {}
-  fn ts_tuple_element(&mut self, n: &AstView::TsTupleElement) {}
-  fn ts_tuple_type(&mut self, n: &AstView::TsTupleType) {}
-  fn ts_type_alias_decl(&mut self, n: &AstView::TsTypeAliasDecl) {}
-  fn ts_type_ann(&mut self, n: &AstView::TsTypeAnn) {}
-  fn ts_type_assertion(&mut self, n: &AstView::TsTypeAssertion) {}
-  fn ts_type_cast_expr(&mut self, n: &AstView::TsTypeCastExpr) {}
-  fn ts_type_lit(&mut self, n: &AstView::TsTypeLit) {}
-  fn ts_type_operator(&mut self, n: &AstView::TsTypeOperator) {}
-  fn ts_type_param(&mut self, n: &AstView::TsTypeParam) {}
-  fn ts_type_param_decl(&mut self, n: &AstView::TsTypeParamDecl) {}
+  fn ts_constructor_type(&mut self, _n: &AstView::TsConstructorType) {}
+  fn ts_enum_decl(&mut self, _n: &AstView::TsEnumDecl) {}
+  fn ts_enum_member(&mut self, _n: &AstView::TsEnumMember) {}
+  fn ts_export_assignment(&mut self, _n: &AstView::TsExportAssignment) {}
+  fn ts_expr_with_type_args(&mut self, _n: &AstView::TsExprWithTypeArgs) {}
+  fn ts_external_module_ref(&mut self, _n: &AstView::TsExternalModuleRef) {}
+  fn ts_fn_type(&mut self, _n: &AstView::TsFnType) {}
+  fn ts_import_equal_decl(&mut self, _n: &AstView::TsImportEqualsDecl) {}
+  fn ts_import_type(&mut self, _n: &AstView::TsImportType) {}
+  fn ts_index_signature(&mut self, _n: &AstView::TsIndexSignature) {}
+  fn ts_indexed_access_type(&mut self, _n: &AstView::TsIndexedAccessType) {}
+  fn ts_infer_type(&mut self, _n: &AstView::TsInferType) {}
+  fn ts_interface_body(&mut self, _n: &AstView::TsInterfaceBody) {}
+  fn ts_interface_decl(&mut self, _n: &AstView::TsInterfaceDecl) {}
+  fn ts_intersection_type(&mut self, _n: &AstView::TsIntersectionType) {}
+  fn ts_keyword_type(&mut self, _n: &AstView::TsKeywordType) {}
+  fn ts_lit_type(&mut self, _n: &AstView::TsLitType) {}
+  fn ts_mapped_type(&mut self, _n: &AstView::TsMappedType) {}
+  fn ts_method_signature(&mut self, _n: &AstView::TsMethodSignature) {}
+  fn ts_module_block(&mut self, _n: &AstView::TsModuleBlock) {}
+  fn ts_module_decl(&mut self, _n: &AstView::TsModuleDecl) {}
+  fn ts_namespace_decl(&mut self, _n: &AstView::TsNamespaceDecl) {}
+  fn ts_namespace_export_decl(&mut self, _n: &AstView::TsNamespaceExportDecl) {}
+  fn ts_non_null_expr(&mut self, _n: &AstView::TsNonNullExpr) {}
+  fn ts_optional_type(&mut self, _n: &AstView::TsOptionalType) {}
+  fn ts_param_prop(&mut self, _n: &AstView::TsParamProp) {}
+  fn ts_parenthesized_type(&mut self, _n: &AstView::TsParenthesizedType) {}
+  fn ts_property_signature(&mut self, _n: &AstView::TsPropertySignature) {}
+  fn ts_qualified_name(&mut self, _n: &AstView::TsQualifiedName) {}
+  fn ts_rest_type(&mut self, _n: &AstView::TsRestType) {}
+  fn ts_this_type(&mut self, _n: &AstView::TsThisType) {}
+  fn ts_tpl_lit_type(&mut self, _n: &AstView::TsTplLitType) {}
+  fn ts_tuple_element(&mut self, _n: &AstView::TsTupleElement) {}
+  fn ts_tuple_type(&mut self, _n: &AstView::TsTupleType) {}
+  fn ts_type_alias_decl(&mut self, _n: &AstView::TsTypeAliasDecl) {}
+  fn ts_type_ann(&mut self, _n: &AstView::TsTypeAnn) {}
+  fn ts_type_assertion(&mut self, _n: &AstView::TsTypeAssertion) {}
+  fn ts_type_cast_expr(&mut self, _n: &AstView::TsTypeCastExpr) {}
+  fn ts_type_lit(&mut self, _n: &AstView::TsTypeLit) {}
+  fn ts_type_operator(&mut self, _n: &AstView::TsTypeOperator) {}
+  fn ts_type_param(&mut self, _n: &AstView::TsTypeParam) {}
+  fn ts_type_param_decl(&mut self, _n: &AstView::TsTypeParamDecl) {}
   fn ts_type_param_instantiation(
     &mut self,
-    n: &AstView::TsTypeParamInstantiation,
+    _n: &AstView::TsTypeParamInstantiation,
   ) {
   }
-  fn ts_type_predicate(&mut self, n: &AstView::TsTypePredicate) {}
-  fn ts_type_query(&mut self, n: &AstView::TsTypeQuery) {}
-  fn ts_type_ref(&mut self, n: &AstView::TsTypeRef) {}
-  fn ts_union_type(&mut self, n: &AstView::TsUnionType) {}
-  fn unary_expr(&mut self, n: &AstView::UnaryExpr) {}
-  fn update_expr(&mut self, n: &AstView::UpdateExpr) {}
-  fn var_decl(&mut self, n: &AstView::VarDecl) {}
-  fn var_declarator(&mut self, n: &AstView::VarDeclarator) {}
-  fn while_stmt(&mut self, n: &AstView::WhileStmt) {}
-  fn with_stmt(&mut self, n: &AstView::WithStmt) {}
-  fn yield_expr(&mut self, n: &AstView::YieldExpr) {}
+  fn ts_type_predicate(&mut self, _n: &AstView::TsTypePredicate) {}
+  fn ts_type_query(&mut self, _n: &AstView::TsTypeQuery) {}
+  fn ts_type_ref(&mut self, _n: &AstView::TsTypeRef) {}
+  fn ts_union_type(&mut self, _n: &AstView::TsUnionType) {}
+  fn unary_expr(&mut self, _n: &AstView::UnaryExpr) {}
+  fn update_expr(&mut self, _n: &AstView::UpdateExpr) {}
+  fn var_decl(&mut self, _n: &AstView::VarDecl) {}
+  fn var_declarator(&mut self, _n: &AstView::VarDeclarator) {}
+  fn while_stmt(&mut self, _n: &AstView::WhileStmt) {}
+  fn with_stmt(&mut self, _n: &AstView::WithStmt) {}
+  fn yield_expr(&mut self, _n: &AstView::YieldExpr) {}
 }
