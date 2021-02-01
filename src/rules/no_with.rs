128ac73620f6a2378031defb994681073ce96e53
Yusuke Tanaka <yusuktan@maguro.dev>
2021-02-01 19:32:46 +0900

    cleanup

--- src/rules/no_with.rs
+++ src/rules/no_with.rs
@@ -2,11 +2,7 @@
 use super::Context;
 use super::LintRule;
 use crate::handler::{Handler, Traverse};
-use dprint_swc_ecma_ast_view::{with_ast_view, SourceFileInfo};
-use swc_ecmascript::ast::{Program, WithStmt};
-use swc_ecmascript::visit::noop_visit_type;
-use swc_ecmascript::visit::Node;
-use swc_ecmascript::visit::Visit;
+use dprint_swc_ecma_ast_view::{with_ast_view, ProgramInfo};
 
 pub struct NoWith;
 
@@ -26,31 +22,30 @@ impl LintRule for NoWith {
     CODE
   }
 
-  fn lint_program(&self, context: &mut Context, program: &Program) {
-    let mut visitor = NoWithVisitor::new(context);
-    visitor.visit_program(program, program);
+  fn lint_program(
+    &self,
+    _context: &mut Context,
+    _program: &swc_ecmascript::ast::Program,
+  ) {
+    unimplemented!();
   }
 
   fn lint_program_with_ast_view(
     &self,
     context: &mut Context,
-    program: &Program,
+    program: &swc_ecmascript::ast::Program,
   ) {
-    if let Program::Module(module) = program {
-      let info = SourceFileInfo {
-        module,
-        source_file: None,
-        tokens: None,
-        comments: None,
-      };
+    let info = ProgramInfo {
+      program,
+      source_file: None,
+      tokens: None,
+      comments: None,
+    };
 
-      with_ast_view(info, |module| {
-        let mut handler = NoWithVisitor::new(context);
-        handler.traverse(module);
-      });
-    } else {
-      self.lint_program(context, program);
-    }
+    with_ast_view(info, |module| {
+      let mut handler = NoWithVisitor::new(context);
+      handler.traverse(module);
+    });
   }
 }
 
@@ -61,14 +56,6 @@ struct NoWithVisitor<'c> {
 impl<'c> NoWithVisitor<'c> {
   fn new(context: &'c mut Context) -> Self {
     Self { context }
-  }
-}
-
-impl<'c> Visit for NoWithVisitor<'c> {
-  noop_visit_type!();
-
-  fn visit_with_stmt(&mut self, with_stmt: &WithStmt, _parent: &dyn Node) {
-    self.context.add_diagnostic(with_stmt.span, CODE, MESSAGE);
   }
 }
 
@@ -89,11 +76,6 @@ mod tests {
     assert_lint_err! {
       NoWith,
       "with (someVar) { console.log('asdf'); }": [{ col: 0, message: MESSAGE }],
-
-      // This is the same as the above one in the sense that what is tested,
-      // but it's present only for with_ast_view testing, which requires that the code is parsed as
-      // Module, not as Script.
-      "with (someVar) { console.log('asdf'); } export const foo = 42;": [{ col: 0, message: MESSAGE }],
     }
   }
 }
