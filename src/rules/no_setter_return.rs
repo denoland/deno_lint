5b9fa9c910d56cc3f0d6dd7df08211f78cf77e26
Yusuke Tanaka <yusuktan@maguro.dev>
2021-03-09 02:20:10 +0900

    test(no-setter-return): add tests for invalid cases

--- src/rules/no_setter_return.rs
+++ src/rules/no_setter_return.rs
@@ -5,6 +5,9 @@ use dprint_swc_ecma_ast_view::{self as AstView, NodeTrait, Spanned};
 
 pub struct NoSetterReturn;
 
+const CODE: &str = "no-setter-return";
+const MESSAGE: &str = "Setter cannot return a value";
+
 impl LintRule for NoSetterReturn {
   fn new() -> Box<Self> {
     Box::new(NoSetterReturn)
@@ -15,7 +18,7 @@ impl LintRule for NoSetterReturn {
   }
 
   fn code(&self) -> &'static str {
-    "no-setter-return"
+    CODE
   }
 
   fn lint_program(&self, _context: &mut Context, _program: ProgramRef<'_>) {
@@ -44,7 +47,7 @@ impl Handler for NoSetterReturnHandler {
       use AstView::Node::*;
       match node {
         SetterProp(_) => true,
-        ClassMethod(ref method) => {
+        ClassMethod(method) => {
           if method.kind() == AstView::MethodKind::Setter {
             true
           } else {
@@ -63,11 +66,7 @@ impl Handler for NoSetterReturnHandler {
     }
 
     if inside_setter(return_stmt.into_node()) {
-      ctx.add_diagnostic(
-        return_stmt.span(),
-        "no-setter-return",
-        "Setter cannot return a value",
-      );
+      ctx.add_diagnostic(return_stmt.span(), CODE, MESSAGE);
     }
   }
 }
@@ -207,14 +206,250 @@ class b {
         {
           line: 4,
           col: 4,
-          message: "Setter cannot return a value",
+          message: MESSAGE,
         },
         {
           line: 7,
           col: 4,
-          message: "Setter cannot return a value",
+          message: MESSAGE,
         }
-      ]
+      ],
+      "({ set a(val){ return val + 1; } })": [
+        {
+          col: 15,
+          message: MESSAGE,
+        }
+      ],
+      "({ set a(val) { return 1; } })": [
+        {
+          col: 16,
+          message: MESSAGE,
+        }
+      ],
+      "class A { set a(val) { return 1; } }": [
+        {
+          col: 23,
+          message: MESSAGE,
+        }
+      ],
+      "class A { static set a(val) { return 1; } }": [
+        {
+          col: 30,
+          message: MESSAGE,
+        }
+      ],
+      "(class { set a(val) { return 1; } })": [
+        {
+          col: 22,
+          message: MESSAGE,
+        }
+      ],
+      "({ set a(val) { return val; } })": [
+        {
+          col: 16,
+          message: MESSAGE,
+        }
+      ],
+      "class A { set a(val) { return undefined; } }": [
+        {
+          col: 23,
+          message: MESSAGE,
+        }
+      ],
+      "(class { set a(val) { return null; } })": [
+        {
+          col: 22,
+          message: MESSAGE,
+        }
+      ],
+      "({ set a(val) { return x + y; } })": [
+        {
+          col: 16,
+          message: MESSAGE,
+        }
+      ],
+      "class A { set a(val) { return foo(); } }": [
+        {
+          col: 23,
+          message: MESSAGE,
+        }
+      ],
+      "(class { set a(val) { return this._a; } })": [
+        {
+          col: 22,
+          message: MESSAGE,
+        }
+      ],
+      "({ set a(val) { return this.a; } })": [
+        {
+          col: 16,
+          message: MESSAGE,
+        }
+      ],
+      "({ set a(val) { if (foo) { return 1; }; } })": [
+        {
+          col: 27,
+          message: MESSAGE,
+        }
+      ],
+      "class A { set a(val) { try { return 1; } catch(e) {} } }": [
+        {
+          col: 29,
+          message: MESSAGE,
+        }
+      ],
+      "(class { set a(val) { while (foo){ if (bar) break; else return 1; } } })": [
+        {
+          col: 56,
+          message: MESSAGE,
+        }
+      ],
+      "({ set a(val) { return 1; }, set b(val) { return 1; } })": [
+        {
+          col: 16,
+          message: MESSAGE,
+        },
+        {
+          col: 42,
+          message: MESSAGE,
+        }
+      ],
+      "class A { set a(val) { return 1; } set b(val) { return 1; } }": [
+        {
+          col: 23,
+          message: MESSAGE,
+        },
+        {
+          col: 48,
+          message: MESSAGE,
+        },
+      ],
+      "(class { set a(val) { return 1; } static set b(val) { return 1; } })": [
+        {
+          col: 22,
+          message: MESSAGE,
+        },
+        {
+          col: 54,
+          message: MESSAGE,
+        },
+      ],
+      "({ set a(val) { if(val) { return 1; } else { return 2 }; } })": [
+        {
+          col: 26,
+          message: MESSAGE,
+        },
+        {
+          col: 45,
+          message: MESSAGE,
+        },
+      ],
+      "class A { set a(val) { switch(val) { case 1: return x; case 2: return y; default: return z } } }": [
+        {
+          col: 45,
+          message: MESSAGE,
+        },
+        {
+          col: 63,
+          message: MESSAGE,
+        },
+        {
+          col: 82,
+          message: MESSAGE,
+        },
+      ],
+      "(class { static set a(val) { if (val > 0) { this._val = val; return val; } return false; } })": [
+        {
+          col: 61,
+          message: MESSAGE,
+        },
+        {
+          col: 75,
+          message: MESSAGE,
+        },
+      ],
+      "({ set a(val) { if(val) { return 1; } else { return; }; } })": [
+        {
+          col: 26,
+          message: MESSAGE,
+        }
+      ],
+      "class A { set a(val) { switch(val) { case 1: return x; case 2: return; default: return z } } }": [
+        {
+          col: 45,
+          message: MESSAGE,
+        },
+        {
+          col: 80,
+          message: MESSAGE,
+        },
+      ],
+      "(class { static set a(val) { if (val > 0) { this._val = val; return; } return false; } })": [
+        {
+          col: 71,
+          message: MESSAGE,
+        }
+      ],
+      "({ set a(val) { function b(){} return b(); } })": [
+        {
+          col: 31,
+          message: MESSAGE,
+        }
+      ],
+      "class A { set a(val) { return () => {}; } }": [
+        {
+          col: 23,
+          message: MESSAGE,
+        }
+      ],
+      "(class { set a(val) { function b(){ return 1; } return 2; } })": [
+        {
+          col: 48,
+          message: MESSAGE,
+        }
+      ],
+      "({ set a(val) { function b(){ return; } return 1; } })": [
+        {
+          col: 40,
+          message: MESSAGE,
+        }
+      ],
+      "class A { set a(val) { var x = function() { return 1; }; return 2; } }": [
+        {
+          col: 57,
+          message: MESSAGE,
+        }
+      ],
+      "(class { set a(val) { var x = () => { return; }; return 2; } })": [
+        {
+          col: 49,
+          message: MESSAGE,
+        }
+      ],
+      "function f(){}; ({ set a(val) { return 1; } });": [
+        {
+          col: 32,
+          message: MESSAGE,
+        }
+      ],
+      "x = function f(){}; class A { set a(val) { return 1; } };": [
+        {
+          col: 43,
+          message: MESSAGE,
+        }
+      ],
+      "x = () => {}; A = class { set a(val) { return 1; } };": [
+        {
+          col: 39,
+          message: MESSAGE,
+        }
+      ],
+      "return; ({ set a(val) { return 1; } }); return 2;": [
+        {
+          col: 24,
+          message: MESSAGE,
+        }
+      ],
     };
   }
 }
