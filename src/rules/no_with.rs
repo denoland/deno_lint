47e54ff8e920ab821419992dc645702a2538b65b
Yusuke Tanaka <yusuktan@maguro.dev>
2021-04-04 21:56:09 +0900

    doc(no-with): add doc

--- src/rules/no_with.rs
+++ src/rules/no_with.rs
@@ -32,6 +32,23 @@ impl LintRule for NoWith {
   ) {
     NoWithHandler.traverse(program, context);
   }
+
+  fn docs(&self) -> &'static str {
+    r#"Disallows the usage of `with` statements.
+
+The `with` statement is discouraged as it may be the source of confusing bugs
+and compatibility issues. For more details, see [with - JavaScript | MDN].
+
+[with - JavaScript | MDN]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with
+
+### Invalid:
+```typescript
+with (someVar) {
+  console.log('foo');
+}
+```
+"#
+  }
 }
 
 struct NoWithHandler;
