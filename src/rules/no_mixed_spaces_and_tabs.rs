a557ca247b693dfdf3bee18c3424185014702296
Yoshiaki Togami <tuabobo123@gmail.com>
2021-06-06 15:41:30 +0900

    doc(no_mixed_spaces_and_tabs): add comment and fix sentences

--- src/rules/no_mixed_spaces_and_tabs.rs
+++ src/rules/no_mixed_spaces_and_tabs.rs
@@ -93,27 +93,26 @@ impl LintRule for NoMixedSpacesAndTabs {
   fn docs(&self) -> &'static str {
     r#"Disallows mixed spaces and tabs for indentation.
 
-Most code rules need to use of tabs or spaces for indentation. Therefore, if a line of code is indented with both tabs and spaces, it will usually result in an error.
+Most code conventions require either tabs or spaces be used for indentation. Therefore, if a line of code is indented with both tabs and spaces, it's most likely a mistake of a developer.
 
 ### Invalid:
 
 ```typescript
-function add(x, y) {
-      return x + y;
+function add(x: number, y: number) {
+    return x + y; // indented with a tab + two spaces
 }
 ```
 
 ```typescript
-    let x = 5,
+    let x = 5, // indented with a tab + two spaces
         y = 7;
-}
 ```
 
 ### Valid:
 
 ```typescript
-function add(x, y) {
-    return x + y;
+function add(x: number, y: number) {
+  return x + y;
 }
 ```"#
   }
