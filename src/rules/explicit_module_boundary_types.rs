a92c17d52360daca67ef299257ba8f39f600bb9d
Yusuke Tanaka <yusuktan@maguro.dev>
2021-07-01 00:33:44 +0900

    fix explicit-module-boundary-types

--- src/rules/explicit_module_boundary_types.rs
+++ src/rules/explicit_module_boundary_types.rs
@@ -63,12 +63,13 @@ it very clear to any users of the module how to supply inputs and handle
 outputs in a type safe manner.
 
 ### Invalid:
+
 ```typescript
 // Missing return type (e.g. void)
 export function printDoc(doc: string, doubleSided: boolean) { return; }
 
 // Missing argument type (e.g. `arg` is of type string)
-export var arrowFn = (arg): string => `hello ${arg}`;
+export const arrowFn = (arg): string => `hello ${arg}`;
 
 // Missing return type (e.g. boolean)
 export function isValid() {
@@ -77,12 +78,13 @@ export function isValid() {
 ```
 
 ### Valid:
+
 ```typescript
 // Typed input parameters and return value
 export function printDoc(doc: string, doubleSided: boolean): void { return; }
 
 // Input of type string and a return value of type string
-export var arrowFn = (arg: string): string => `hello ${arg}`;
+export const arrowFn = (arg: string): string => `hello ${arg}`;
 
 // Though lacking a return type, this is valid as it is not exported
 function isValid() {
