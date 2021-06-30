709e6be38e450579e38f67babab2df8472dcaf02
Yusuke Tanaka <yusuktan@maguro.dev>
2021-07-01 00:35:52 +0900

    fix getter-return

--- src/rules/getter_return.rs
+++ src/rules/getter_return.rs
@@ -65,27 +65,29 @@ Getter functions return the value of a property.  If the function returns no
 value then this contract is broken.
 
 ### Invalid:
+
 ```typescript
-let foo = { 
+let foo = {
   get bar() {}
 };
 
-class Person { 
+class Person {
   get name() {}
 }
 ```
-    
+
 ### Valid:
+
 ```typescript
-let foo = { 
-  get bar() { 
-    return true; 
+let foo = {
+  get bar() {
+    return true;
   }
 };
 
-class Person { 
-  get name() { 
-    return "alice"; 
+class Person {
+  get name() {
+    return "alice";
   }
 }
 ```
