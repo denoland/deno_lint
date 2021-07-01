01f5488c03556c21317767f89fbcefdb3d0617a9
Yusuke Tanaka <yusuktan@maguro.dev>
2021-07-01 23:19:06 +0900

    fix no-compare-neg-zero

--- src/rules/no_compare_neg_zero.rs
+++ src/rules/no_compare_neg_zero.rs
@@ -58,11 +58,13 @@ impl LintRule for NoCompareNegZero {
 Comparing a value directly against negative may not work as expected as it will also pass for non-negative zero (i.e. `0` and `+0`). Explicit comparison with negative zero can be performed using `Object.is`.
 
 ### Invalid:
+
 ```typescript
 if (x === -0) {}
 ```
 
 ### Valid:
+
 ```typescript
 if (x === 0) {}
 
