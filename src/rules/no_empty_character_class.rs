9085465045840a30973f3d45a5fa1c1390a0d48d
Yusuke Tanaka <yusuktan@maguro.dev>
2021-07-01 23:47:34 +0900

    fix no-empty-character-class

--- src/rules/no_empty_character_class.rs
+++ src/rules/no_empty_character_class.rs
@@ -44,14 +44,16 @@ impl LintRule for NoEmptyCharacterClass {
 Regular expression character classes are a series of characters in brackets, e.g. `[abc]`.
 if nothing is supplied in the brackets it will not match anything which is likely
 a typo or mistake.
-    
+
 ### Invalid:
+
 ```typescript
 /^abc[]/.test("abcdefg");  // false, as `d` does not match an empty character class
 "abcdefg".match(/^abc[]/); // null
 ```
 
 ### Valid:
+
 ```typescript
 // Without a character class
 /^abc/.test("abcdefg"); // true
@@ -59,7 +61,8 @@ a typo or mistake.
 
 // With a valid character class
 /^abc[a-z]/.test("abcdefg"); // true
-"abcdefg".match(/^abc[a-z]/); // ["abcd"]```
+"abcdefg".match(/^abc[a-z]/); // ["abcd"]
+```
 "#
   }
 }
