ef0498fa180f331e8d82dbd4d1c1ae917d0b5b80
Yusuke Tanaka <yusuktan@maguro.dev>
2021-07-01 23:16:15 +0900

    fix no-class-assign

--- src/rules/no_class_assign.rs
+++ src/rules/no_class_assign.rs
@@ -41,7 +41,7 @@ impl LintRule for NoClassAssign {
   fn docs(&self) -> &'static str {
     r#"Disallows modifying variables of class declarations
 
-Declaring a class such as `class A{}`, creates a variable `A`.  Like any variable
+Declaring a class such as `class A {}`, creates a variable `A`.  Like any variable
 this can be modified or reassigned. In most cases this is a mistake and not what
 was intended.
 
@@ -50,7 +50,7 @@ was intended.
 class A {}
 A = 0;  // reassigning the class variable itself
 ```
-    
+
 ### Valid:
 ```typescript
 class A{}
