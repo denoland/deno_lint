ef5fe77d2975e8996c33e772c6dc82ceef14498e
Yusuke Tanaka <yusuktan@maguro.dev>
2021-07-10 17:40:15 +0900

    docs(no-octal): add docs

--- src/rules/no_octal.rs
+++ src/rules/no_octal.rs
@@ -35,6 +35,33 @@ impl LintRule for NoOctal {
       ProgramRef::Script(ref s) => visitor.visit_script(s, &DUMMY_NODE),
     }
   }
+
+  fn docs(&self) -> &'static str {
+    r#"Disallows expressing octal numbers via numeric literals beginning with `0`
+
+Octal numbers can be expressed via numeric literals with leading `0` like `042`,
+but this expression often confuses programmers. That's why ECMAScript's strict
+mode throws `SyntaxError` for the expression.
+
+Since ES2015, the other prefix `0o` has been introduced as an alternative. This
+new one is always encouraged to use in today's code.
+
+### Invalid:
+
+```typescript
+const a = 042;
+const b = 7 + 042;
+```
+
+### Valid:
+
+```typescript
+const a = 0o42;
+const b = 7 + 0o42;
+const c = "042";
+```
+"#
+  }
 }
 
 struct NoOctalVisitor<'c, 'view> {
