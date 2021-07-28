3f07ba78529d17de40573e83294885104a8752a3
Yusuke Tanaka <yusuktan@maguro.dev>
2021-07-28 22:57:51 +0900

    docs(no-redeclare): add docs

--- src/rules/no_redeclare.rs
+++ src/rules/no_redeclare.rs
@@ -40,6 +40,49 @@ impl LintRule for NoRedeclare {
       ProgramRef::Script(ref s) => visitor.visit_script(s, &DUMMY_NODE),
     }
   }
+
+  fn docs(&self) -> &'static str {
+    r#"Disallows redeclaration of variables.
+
+JavaScript allows us to redeclare variables with the same name using `var`, but
+redeclaration should not be used since it can make variables hard to trace.
+
+In addition, this lint rule disallows redeclaration using `let` or `const` as
+well, although ESLint allows. This is useful because we can notice a syntax
+error before actually running the code.
+
+### Invalid:
+
+```typescript
+var a = 3;
+var a = 10;
+
+let b = 3;
+let b = 10;
+
+const c = 3;
+const c = 10;
+
+function d() {}
+function d() {}
+```
+
+### Valid:
+
+```typescript
+var a = 3;
+function f() {
+  var a = 10;
+}
+
+if (foo) {
+  let b = 2;
+} else {
+  let b = 3;
+}
+```
+"#
+  }
 }
 
 struct NoRedeclareVisitor<'c, 'view> {
