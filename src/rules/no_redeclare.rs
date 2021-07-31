8960c8ab7bbbd8e10bbecf29f0d2d8cdc5137acf
Yusuke Tanaka <yusuktan@maguro.dev>
2021-07-31 11:48:08 +0900

    fix doc

--- src/rules/no_redeclare.rs
+++ src/rules/no_redeclare.rs
@@ -42,7 +42,8 @@ impl LintRule for NoRedeclare {
   }
 
   fn docs(&self) -> &'static str {
-    r#"Disallows redeclaration of variables.
+    r#"Disallows redeclaration of variables, functions, parameters with the same
+name.
 
 JavaScript allows us to redeclare variables with the same name using `var`, but
 redeclaration should not be used since it can make variables hard to trace.
@@ -50,6 +51,10 @@ redeclaration should not be used since it can make variables hard to trace.
 In addition, this lint rule disallows redeclaration using `let` or `const` as
 well, although ESLint allows. This is useful because we can notice a syntax
 error before actually running the code.
+
+As for functions and parameters, JavaScript just treats these as runtime errors,
+throwing `SyntaxError` when being run. It's also beneficial to detect this sort
+of errors statically.
 
 ### Invalid:
 
@@ -65,6 +70,12 @@ const c = 10;
 
 function d() {}
 function d() {}
+
+function e(arg: number) {
+  var arg: number;
+}
+
+function f(arg: number, arg: string) {}
 ```
 
 ### Valid:
@@ -193,6 +204,7 @@ mod tests {
       "var a = 3; var a = 10; var a = 15;": [{col: 15, message: MESSAGE}, {col: 27, message: MESSAGE}],
       "var a; var {a = 0, b: Object = 0} = {};": [{line: 1, col: 12, message: MESSAGE}],
       "var a; var {a = 0, b: globalThis = 0} = {};": [{line: 1, col: 12, message: MESSAGE}],
+      "function f(foo: number, foo: string) {}": [{line: 1, col: 24, message: MESSAGE}],
     }
   }
 }
