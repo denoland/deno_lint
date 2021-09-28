74b7de1af3593cc673385924ee060112d9b9199a
Satya Rohith <me@satyarohith.com>
2021-09-28 08:18:57 +0530

    fix: pin fresh to a commit (#920)

--- www/deps.ts
+++ www/deps.ts
@@ -1,7 +1,7 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/runtime.ts";
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/a929b1022ab541e937f94592ecce05e7f4ffdaef/runtime.ts";
 import MarkdownIt from "https://dev.jspm.io/markdown-it@12.0.4";
 export { MarkdownIt };
-import { IS_BROWSER } from "https://raw.githubusercontent.com/lucacasonato/fresh/main/runtime.ts";
+import { IS_BROWSER } from "https://raw.githubusercontent.com/lucacasonato/fresh/a929b1022ab541e937f94592ecce05e7f4ffdaef/runtime.ts";
 import { setup, tw } from "https://esm.sh/twind";
 
 export { setup, tw };
--- www/main.ts
+++ www/main.ts
@@ -3,7 +3,7 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
-import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/main/server.ts";
+import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/a929b1022ab541e937f94592ecce05e7f4ffdaef/server.ts";
 import routes from "./routes.gen.ts";
 
 start(routes);
