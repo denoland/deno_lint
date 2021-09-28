d6e0c8d183c6eff44d01c3fb678d00394147ab17
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-09-28 02:25:12 +0200

    Revert "fix(www): use fixes version of fresh (#915)"
    This reverts commit 4c461c7ad088540c5bcb5fc54b70ae4d234c1f67.

--- www/server_deps.ts
+++ www/server_deps.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/a929b1022ab541e937f94592ecce05e7f4ffdaef/server.ts";
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/main/server.ts";
