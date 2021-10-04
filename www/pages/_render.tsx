df634781dd80906c406012134016c33c1b09b044
Luca Casonato <hello@lcas.dev>
2021-10-04 13:15:36 +0200

    remove stale code

--- www/pages/_render.tsx
+++ www/pages/_render.tsx
@@ -40,31 +40,3 @@ export function render(ctx: RenderContext, render: RenderFn) {
   const newSnapshot = sheet.reset();
   ctx.state.set("twindSnapshot", newSnapshot);
 }
-
-// export function postRender(ctx: RenderContext) {
-//   // do normal stuff
-//   ctx.head.push(
-//     h("link", {
-//       href:
-//         "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.2.1/build/styles/monokai-sublime.min.css",
-//       rel: "stylesheet",
-//     }),
-//   );
-//   ctx.head.push(
-//     h("script", {
-//       src:
-//         "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.2.1/build/highlight.min.js",
-//     }),
-//   );
-
-//   // do the twind stuff
-//   const snapshot = ctx.state.get("twindSnapshot") as unknown[] | null;
-//   if (snapshot !== null) {
-//     sheet.reset(snapshot);
-//     const { id, textContent } = getStyleTagProperties(sheet);
-//     ctx.head.push(
-//       h("style", { id, dangerouslySetInnerHTML: { __html: textContent } }),
-//     );
-//   }
-//   sheet.reset(initial);
-// }
