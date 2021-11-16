a064da248e0845379a0686f5934eefac47227f1c
ah-yu <zheyuzhang03@gmail.com>
2021-11-16 19:08:00 +0800

    check jsdoc comment on class/interface

--- docs/rules/require_jsdoc.md
+++ docs/rules/require_jsdoc.md
@@ -1,11 +1,24 @@
 Disallow exported functions that have no JSDoc
 
-This lint rule aims to ensure a JSDoc exists for exported apis
+This lint rule aims to check for presence of jsdoc comments on public apis, such
+as class declarations, interface declarations and functions.
 
 ### Invalid:
 
 ```typescript
-export function f() {}
+export function f() {} // missing jsdoc comment on function f
+
+export class A { // missing jsdoc comment on class A
+  constructor() {} // missing jsdoc comment on constructor
+
+  method() {} // missing jsdoc comment on method
+}
+
+export interface B { // missing jsdoc comment on interface B
+  bar(): void; // missing jsdoc comment on method bar
+}
+
+export default () => {}; // missing jsdoc comment on this function
 ```
 
 ### Valid:
@@ -13,4 +26,19 @@ export function f() {}
 ```typescript
 /** doc */
 export function f() {}
+
+/** doc */
+export class A {
+  /** doc */
+  constructor() {}
+
+  /** doc */
+  method() {}
+}
+
+/** doc */
+export interface A {
+  /** doc */
+  method(): void;
+}
 ```
--- src/rules/require_jsdoc.rs
+++ src/rules/require_jsdoc.rs
@@ -2,16 +2,20 @@
 use super::{Context, LintRule};
 use crate::handler::{Handler, Traverse};
 use crate::{Program, ProgramRef};
-use deno_ast::swc::common::comments::{Comment, CommentKind};
+use deno_ast::swc::common::comments::CommentKind;
 use deno_ast::swc::common::Spanned;
-use deno_ast::view::{self as ast_view, Decl, DefaultDecl};
+use deno_ast::view::{
+  self as ast_view, Class, ClassMember, Decl, DefaultDecl, Expr,
+  TsInterfaceDecl, TsTypeElement,
+};
+
 use std::sync::Arc;
 
 #[derive(Debug)]
 pub struct RequireJsdoc;
 
 const CODE: &str = "require-jsdoc";
-const MESSAGE: &str = "exported function has no JSDoc";
+const MESSAGE: &str = "Missing jsdoc comment";
 
 impl LintRule for RequireJsdoc {
   fn new() -> Arc<Self> {
@@ -44,10 +48,17 @@ struct RequireJsdocHandler;
 
 impl Handler for RequireJsdocHandler {
   fn export_decl(&mut self, _n: &ast_view::ExportDecl, _ctx: &mut Context) {
-    if let Decl::Fn(_) = _n.decl {
-      if !check_jsdoc_exsit(_ctx.leading_comments_at(_n.span().lo)) {
-        _ctx.add_diagnostic(_n.span(), CODE, MESSAGE);
+    match _n.decl {
+      Decl::Fn(_) => check_jsdoc(_n, _ctx),
+      Decl::TsInterface(interface_decl) => {
+        check_jsdoc(_n, _ctx);
+        check_interface_decl(interface_decl, _ctx);
       }
+      Decl::Class(class_decl) => {
+        check_jsdoc(_n, _ctx);
+        check_class(class_decl.class, _ctx);
+      }
+      _ => {}
     }
   }
 
@@ -56,21 +67,58 @@ impl Handler for RequireJsdocHandler {
     _n: &ast_view::ExportDefaultDecl,
     _ctx: &mut Context,
   ) {
-    if let DefaultDecl::Fn(_) = _n.decl {
-      if !check_jsdoc_exsit(_ctx.leading_comments_at(_n.span().lo)) {
-        _ctx.add_diagnostic(_n.span(), CODE, MESSAGE);
+    match _n.decl {
+      DefaultDecl::Fn(_) => check_jsdoc(_n, _ctx),
+      DefaultDecl::Class(class_expr) => {
+        check_jsdoc(_n, _ctx);
+        check_class(class_expr.class, _ctx);
       }
+      DefaultDecl::TsInterfaceDecl(interface_decl) => {
+        check_jsdoc(_n, _ctx);
+        check_interface_decl(interface_decl, _ctx);
+      }
+    }
+  }
+
+  fn export_default_expr(
+    &mut self,
+    _n: &ast_view::ExportDefaultExpr,
+    _ctx: &mut Context,
+  ) {
+    if let Expr::Arrow(_) = _n.expr {
+      check_jsdoc(_n, _ctx)
     }
   }
 }
 
-fn check_jsdoc_exsit<'c>(
-  mut comments: impl Iterator<Item = &'c Comment>,
-) -> bool {
-  comments.any(|comment| match comment.kind {
-    CommentKind::Block => comment.text.starts_with('*'),
-    _ => false,
+fn check_class(class: &Class, ctx: &mut Context) {
+  class.body.iter().for_each(|member| match member {
+    ClassMember::Method(_) | ClassMember::Constructor(_) => {
+      check_jsdoc(member, ctx)
+    }
+    _ => {}
   })
+}
+
+fn check_interface_decl(interface_decl: &TsInterfaceDecl, ctx: &mut Context) {
+  interface_decl.body.body.iter().for_each(|type_element| {
+    if let TsTypeElement::TsMethodSignature(_) = type_element {
+      check_jsdoc(type_element, ctx)
+    }
+  });
+}
+
+fn check_jsdoc(n: impl Spanned, ctx: &mut Context) {
+  let exists =
+    ctx
+      .leading_comments_at(n.span().lo)
+      .any(|comment| match comment.kind {
+        CommentKind::Block => comment.text.starts_with('*'),
+        _ => false,
+      });
+  if !exists {
+    ctx.add_diagnostic(n.span(), CODE, MESSAGE);
+  }
 }
 
 #[cfg(test)]
@@ -81,8 +129,39 @@ mod tests {
   fn require_jsdoc_valid() {
     assert_lint_ok! {
       RequireJsdoc,
-      r#"/** doc */ export function test() {}"#,
+      r#"/** doc */ export function a() {}"#,
       r#"/** doc */ export default function() {}"#,
+      r#"/** doc */ export class A {
+        /** doc */
+        constructor() { }
+        /** doc */  
+        b() {}
+        #d() {}
+      }"#,
+      r#"/** doc */ export interface A {
+        /** doc */
+        b(): string;
+      }"#,
+      r#"/** doc */ export default class {
+        /** doc */
+        constructor() { }
+        /** doc */  
+        b() {}
+        #d() {}
+      }"#,
+      r#"/** doc */ export default interface A {
+        /** doc */
+        b(): string;
+      }"#,
+      r#"/** doc */ export default function a() {}"#,
+      r#"/** doc */ export default () => {}"#,
+      r#"/** doc */ export default class A {
+        /** doc */
+        constructor() { }
+        /** doc */  
+        b() {}
+        #d() {}
+      }"#,
     };
   }
 
@@ -91,7 +170,36 @@ mod tests {
     assert_lint_err! {
       RequireJsdoc,
       r#"export function test() {}"#: [{col: 0, message:MESSAGE}],
+      r#"export class A{ constructor(){} b(){}}"#: [
+        {col: 0, message:MESSAGE},
+        {col:16, message:MESSAGE},
+        {col:32, message:MESSAGE}
+      ],
+      r#"export interface A{ bar(): string;}"#: [
+        {col: 0, message:MESSAGE},
+        {col: 20, message:MESSAGE},
+      ],
       r#"export default function() {}"#: [{col: 0, message:MESSAGE}],
+      r#"export default class{ constructor(){} b(){}}"#: [
+        {col: 0, message:MESSAGE},
+        {col:22, message:MESSAGE},
+        {col:38, message:MESSAGE}
+      ],
+      r#"export default class A{ constructor(){} b(){}};"#: [
+        {col: 0, message:MESSAGE},
+        {col: 24, message:MESSAGE},
+        {col: 40, message:MESSAGE},
+      ],
+      r#"export default interface A{ bar(): string;}"#: [
+        {col: 0, message:MESSAGE},
+        {col: 28, message:MESSAGE},
+      ],
+      r#"export default () => {};"#: [
+        {col: 0, message:MESSAGE},
+      ],
+      r#"export default function a(){};"#: [
+        {col: 0, message:MESSAGE},
+      ],
     }
   }
 }
