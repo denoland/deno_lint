6f72d03cc018356f586325317bd3a1ccaa597b5a
Bartek Iwańczuk <biwanczuk@gmail.com>
2022-04-07 16:16:34 +0200

    temp

--- www/server_deps.ts
+++ www/server_deps.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/d1076b0ad1b420aec689324b3342e543c0d5a591/server.ts";
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/3d63efc7a8702cdffa4d00b3d2519ea3bd338208/server.ts";
