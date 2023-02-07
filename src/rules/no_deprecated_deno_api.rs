0afc178f425433849f4a3bbb4304fd545d908af2
Kenta Moriuchi <moriken@kimamass.com>
2023-02-07 10:43:38 +0900

    update(no-deprecated-deno-api): deprecate `Deno.run` (#1128)

--- src/rules/no_deprecated_deno_api.rs
+++ src/rules/no_deprecated_deno_api.rs
@@ -78,6 +78,7 @@ enum DeprecatedApi {
   IterSync,
   ReadAll,
   ReadAllSync,
+  Run,
   WriteAll,
   WriteAllSync,
 }
@@ -105,6 +106,7 @@ impl TryFrom<(&str, &str)> for DeprecatedApi {
       "File" => Ok(DeprecatedApi::File),
       "readAll" => Ok(DeprecatedApi::ReadAll),
       "readAllSync" => Ok(DeprecatedApi::ReadAllSync),
+      "run" => Ok(DeprecatedApi::Run),
       "writeAll" => Ok(DeprecatedApi::WriteAll),
       "writeAllSync" => Ok(DeprecatedApi::WriteAllSync),
       _ => Err(()),
@@ -145,12 +147,14 @@ impl DeprecatedApi {
       File => "Deno.File",
       ReadAll => "Deno.readAll",
       ReadAllSync => "Deno.readAllSync",
+      Run => "Deno.run",
       WriteAll => "Deno.writeAll",
       WriteAllSync => "Deno.writeAllSync",
     }
   }
 
   fn get_replacement(&self) -> Replacement {
+    const DENO_API: &str = "https://deno.land/api";
     const BUFFER_TS: &str = "https://deno.land/std/io/buffer.ts";
     const STREAMS_TS: &str = "https://deno.land/std/streams/conversion.ts";
 
@@ -165,6 +169,7 @@ impl DeprecatedApi {
       File => Name("Deno.FsFile"),
       ReadAll => NameAndUrl("readAll", STREAMS_TS),
       ReadAllSync => NameAndUrl("readAllSync", STREAMS_TS),
+      Run => NameAndUrl("Deno.Command", DENO_API),
       WriteAll => NameAndUrl("writeAll", STREAMS_TS),
       WriteAllSync => NameAndUrl("writeAllSync", STREAMS_TS),
     }
@@ -376,6 +381,13 @@ mod tests {
           col: 10,
           message: File.message(),
           hint: File.hint()
+        }
+      ],
+      "Deno.run(options);": [
+        {
+          col: 0,
+          message: Run.message(),
+          hint: Run.hint()
         }
       ],
 
