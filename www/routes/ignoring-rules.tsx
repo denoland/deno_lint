20fa6cacd25492b05d1205d92de76f08fbed303c
Yusuke Tanaka <yusuktan@maguro.dev>
2023-02-15 23:56:43 +0900

    use relative path from ignorint-rules.tsx to point to ignoring-rules.md

--- www/routes/ignoring-rules.tsx
+++ www/routes/ignoring-rules.tsx
@@ -3,10 +3,12 @@ import { Header } from "../components/Header.tsx";
 import { CommonHead } from "../components/CommonHead.tsx";
 import type { Handlers, PageProps } from "$fresh/server.ts";
 import { renderMarkdown } from "../utils/render_markdown.ts";
+import { join, fromFileUrl } from "https://deno.land/std@0.177.0/path/posix.ts";
 
 export const handler: Handlers<string> = {
   async GET(_req, ctx) {
-    const md = await Deno.readTextFile("static/ignoring-rules.md");
+    const mdPath = join(fromFileUrl(import.meta.url), "../../static/ignoring-rules.md");
+    const md = await Deno.readTextFile(mdPath);
     const html = renderMarkdown(md);
     return ctx.render(html);
   },
