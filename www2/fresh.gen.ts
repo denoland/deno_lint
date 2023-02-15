5359f76350d33dbd83d19ebe064df36ffd55f4aa
Yusuke Tanaka <yusuktan@maguro.dev>
2023-02-15 23:01:12 +0900

    remove unused files

--- www2/fresh.gen.ts
+++ www2/fresh.gen.ts
@@ -3,22 +3,15 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/ignoring-rules.tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from "./routes/ignoring-rules.tsx";
+import * as $1 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/ignoring-rules.tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/ignoring-rules.tsx": $0,
+    "./routes/index.tsx": $1,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
   config,
 };
