denoland/deno_lint

commit a52ef5f58dbd347a9a1db31e5449cb276290c625

fix: Fix website background color (#1133)

www/pages/ignoring-rules.tsx
@@ -8,7 +8,7 @@ function IgnoringRulesPage() {
   const html = renderMarkdown(md);
 
   return (
-    <div class={tw`dark:bg-[#0d1117] dark:text-white py-6 h-screen`}>
+    <div class={tw`dark:bg-[#0d1117] dark:text-white py-6 h-full`}>
       <div
         class={tw`mx-auto max-w-screen-md px-6 sm:px-6 md:px-8 `}
       >

www/pages/index.tsx
@@ -39,7 +39,7 @@ function IndexPage(props: PageProps) {
     .filter((rule: RuleData) => rule.code.includes(search));
 
   return (
-    <div class={tw`dark:bg-[#0d1117] dark:text-white py-6 h-screen`}>
+    <div class={tw`dark:bg-[#0d1117] dark:text-white py-6 h-full`}>
       <div class={tw`mx-auto max-w-screen-md px-6 sm:px-6 md:px-8`}>
         <Head>
           <link

commit eb8b6a48baa959756d040f671afb664744c15b01

fix(www): update fresh to latest & fix the appearance issue (#1134)
This commit upgrade fresh to the latest version as well as fixes a background color issue that was introduced in #1133.

.github/workflows/ci.yml
@@ -64,9 +64,9 @@ jobs:
         if: contains(matrix.os, 'ubuntu')
         run: deno run --allow-run ./tools/lint.ts --release
 
-      - name: Check if www/public/docs.json is up-to-date
+      - name: Check if www/static/docs.json is up-to-date
         if: contains(matrix.os, 'ubuntu')
-        run: diff <(./target/release/examples/dlint rules --json) www/public/docs.json
+        run: diff <(./target/release/examples/dlint rules --json) www/static/docs.json
 
       - name: Benchmarks
         if: contains(matrix.os, 'ubuntu')

.gitignore
@@ -1,5 +1,3 @@
 .idea
 
 /target
-
-docs.json

www/components/CommonHead.tsx
@@ -1,0 +1,15 @@
+import { Head } from "$fresh/runtime.ts";
+import { JSX } from "preact";
+
+export function CommonHead() {
+  return (
+    <Head>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/gh/lucacasonato/manual@df7ae27/www/static/markdown.css"
+        crossOrigin="anonymous"
+      />
+      <link rel="stylesheet" href="extension.css" />
+    </Head>
+  );
+}

www/components/Header.tsx
@@ -1,29 +1,28 @@
-/** @jsx h */
-import { h, tw } from "../deps.ts";
+import { JSX } from "preact";
 
 export function Header() {
   return (
-    <section class={tw`my-8`}>
-      <h1 class={tw`text-3xl font-bold`}>deno_lint docs</h1>
-      <div class={tw`flex flex-wrap justify-between w-full`}>
-        <div class={tw`mt-2`}>
+    <section class="my-8">
+      <h1 class="text-3xl font-bold">deno_lint docs</h1>
+      <div class="flex flex-wrap justify-between w-full">
+        <div class="mt-2">
           <a
             href="/"
-            class={tw`hover:underline`}
+            class="hover:underline"
           >
             Rule overview
           </a>
           <a
             href="/ignoring-rules"
-            class={tw`hover:underline ml-4`}
+            class="hover:underline ml-4"
           >
             Ignoring rules
           </a>
         </div>
-        <div class={tw`mt-2`}>
+        <div class="mt-2">
           <a
             href="https://github.com/denoland/deno_lint"
-            class={tw`hover:underline`}
+            class="hover:underline"
           >
             View on GitHub
           </a>

www/deps.ts
@@ -1,18 +1,0 @@
-// fresh
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/d1076b0ad1b420aec689324b3342e543c0d5a591/runtime.ts";
-
-// marked
-export { default as marked } from "https://esm.sh/marked@3.0.4";
-
-// prism
-export { default as Prism } from "https://esm.sh/prismjs@1.25.0?pin=v57";
-import "https://esm.sh/prismjs@1.25.0/components/prism-javascript.js?no-check&pin=v57";
-import "https://esm.sh/prismjs@1.25.0/components/prism-typescript.js?no-check&pin=v57";
-
-// twind
-import { setup, tw } from "https://esm.sh/twind@0.16.16";
-export {
-  getStyleTagProperties,
-  virtualSheet,
-} from "https://esm.sh/twind@0.16.16/sheets";
-export { setup, tw };

www/dev.ts
@@ -1,0 +1,5 @@
+#!/usr/bin/env -S deno run -A --watch=static/,routes/
+
+import dev from "$fresh/dev.ts";
+
+await dev(import.meta.url, "./main.ts");

www/fresh.gen.ts
@@ -1,0 +1,19 @@
+// DO NOT EDIT. This file is generated by fresh.
+// This file SHOULD be checked into source version control.
+// This file is automatically updated during development when running `dev.ts`.
+
+import config from "./deno.json" assert { type: "json" };
+import * as $0 from "./routes/ignoring-rules.tsx";
+import * as $1 from "./routes/index.tsx";
+
+const manifest = {
+  routes: {
+    "./routes/ignoring-rules.tsx": $0,
+    "./routes/index.tsx": $1,
+  },
+  islands: {},
+  baseUrl: import.meta.url,
+  config,
+};
+
+export default manifest;

www/main.ts
@@ -3,9 +3,11 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-/// <reference lib="deno.unstable" />
 
-import { start } from "./server_deps.ts";
-import routes from "./routes.gen.ts";
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
 
-start(routes);
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)] });

www/pages/_render.tsx
@@ -1,39 +1,0 @@
-// This module adds twind support.
-
-import { setup, virtualSheet } from "../deps.ts";
-import { RenderContext, RenderFn } from "../server_deps.ts";
-
-const sheet = virtualSheet();
-sheet.reset();
-setup({
-  sheet,
-  darkMode: "media",
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "-apple-system",
-          "BlinkMacSystemFont",
-          '"Segoe UI"',
-          '"Roboto"',
-          '"Oxygen"',
-          '"Ubuntu"',
-          '"Cantarell"',
-          '"Fira Sans"',
-          '"Droid Sans"',
-          '"Helvetica Neue"',
-          "sans-serif",
-        ],
-      },
-    },
-  },
-});
-
-export function render(ctx: RenderContext, render: RenderFn) {
-  const snapshot = ctx.state.get("twindSnapshot") as unknown[] | null;
-  sheet.reset(snapshot || undefined);
-  render();
-  ctx.styles.splice(0, ctx.styles.length, ...sheet.target);
-  const newSnapshot = sheet.reset();
-  ctx.state.set("twindSnapshot", newSnapshot);
-}

www/pages/ignoring-rules.tsx
@@ -1,32 +1,0 @@
-/** @jsx h */
-import { h, Head, tw, useData } from "../deps.ts";
-import { Header } from "../components/Header.tsx";
-import { diskFetcher, renderMarkdown } from "../components/utils.ts";
-
-function IgnoringRulesPage() {
-  const md = useData("www/public/ignoring-rules.md", diskFetcher);
-  const html = renderMarkdown(md);
-
-  return (
-    <div class={tw`dark:bg-[#0d1117] dark:text-white py-6 h-full`}>
-      <div
-        class={tw`mx-auto max-w-screen-md px-6 sm:px-6 md:px-8 `}
-      >
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/gh/lucacasonato/manual@df7ae27/www/static/markdown.css"
-            crossOrigin="anonymous"
-          />
-        </Head>
-        <Header />
-        <main
-          dangerouslySetInnerHTML={{ __html: html }}
-          class={tw`markdown-body my-8`}
-        />
-      </div>
-    </div>
-  );
-}
-
-export default IgnoringRulesPage;

www/public/extension.css
@@ -1,21 +1,0 @@
-h3#invalid ~ pre:not(h3#valid ~ pre){
-    background-color: #f4e7e7;
-    border: 2px solid #ec8282;
-}
-
-h3#valid ~ pre {
-    background-color: #e9f0db;
-    border: 2px solid #72c072;
-}
-
-@media (prefers-color-scheme: dark) {
-    h3#invalid ~ pre:not(h3#valid ~ pre){
-        background-color: #221616;
-        border: 2px solid #aa3f3f;
-    }
-    
-    h3#valid ~ pre {
-        background-color: #1b2216;
-        border: 2px solid #406e40;
-    }
-}

www/routes.gen.ts
@@ -1,18 +1,0 @@
-// DO NOT EDIT. This file is generated by `fresh`.
-// This file SHOULD be checked into source version control.
-// To update this file, run `fresh routes`.
-
-import * as $0 from "./pages/index.tsx";
-import * as $1 from "./pages/ignoring-rules.tsx";
-import * as $2 from "./pages/_render.tsx";
-
-const routes = {
-  pages: {
-    "./pages/index.tsx": $0,
-    "./pages/ignoring-rules.tsx": $1,
-    "./pages/_render.tsx": $2,
-  },
-  baseUrl: import.meta.url,
-};
-
-export default routes;

www/routes/ignoring-rules.tsx
@@ -1,0 +1,32 @@
+import { Head } from "$fresh/runtime.ts";
+import { Header } from "../components/Header.tsx";
+import { CommonHead } from "../components/CommonHead.tsx";
+import type { Handlers, PageProps } from "$fresh/server.ts";
+import { renderMarkdown } from "../utils/render_markdown.ts";
+import { join, fromFileUrl } from "https://deno.land/std@0.177.0/path/posix.ts";
+
+export const handler: Handlers<string> = {
+  async GET(_req, ctx) {
+    const mdPath = join(fromFileUrl(import.meta.url), "../../static/ignoring-rules.md");
+    const md = await Deno.readTextFile(mdPath);
+    const html = renderMarkdown(md);
+    return ctx.render(html);
+  },
+};
+
+export default function IgnoringRulesPage(props: PageProps<string>) {
+  return (
+    <div class="py-6">
+      <div
+        class="mx-auto max-w-screen-md px-6 sm:px-6 md:px-8"
+      >
+        <CommonHead />
+        <Header />
+        <main
+          dangerouslySetInnerHTML={{ __html: props.data }}
+          class="markdown-body my-8"
+        />
+      </div>
+    </div>
+  );
+}

www/routes/index.tsx
@@ -1,8 +1,9 @@
-/** @jsx h */
-/** @jsxFrag Fragment */
-import { Fragment, h, Head, PageProps, tw, useData } from "../deps.ts";
+import { Head } from "$fresh/runtime.ts";
+import jsonData from "../static/docs.json" assert { type: "json" };
 import { Header } from "../components/Header.tsx";
-import { diskFetcher, renderMarkdown } from "../components/utils.ts";
+import { CommonHead } from "../components/CommonHead.tsx";
+import type { Handlers, PageProps } from "$fresh/server.ts";
+import { renderMarkdown } from "../utils/render_markdown.ts";
 
 interface RuleData {
   code: string;
@@ -11,19 +12,20 @@ interface RuleData {
   tags: string[];
 }
 
-function IndexPage(props: PageProps) {
-  const jsonData: any[] = JSON.parse(
-    useData("www/public/docs.json", diskFetcher),
-  );
+export const handler: Handlers<RuleData[]> = {
+  async GET(_req, ctx) {
+    const rules = jsonData.map<RuleData>((rule) => ({
+      code: rule.code,
+      snippet: renderMarkdown(rule.docs.split("\n")[0]),
+      docs: renderMarkdown(rule.docs.split("\n").slice(1).join("\n")),
+      tags: rule.tags,
+    }));
+    return ctx.render(rules);
+  }
+};
 
-  const style = useData("www/public/extension.css", diskFetcher);
-
-  const rules = jsonData.map<RuleData>((rule) => ({
-    code: rule.code,
-    snippet: renderMarkdown(rule.docs.split("\n")[0]),
-    docs: renderMarkdown(rule.docs.split("\n").slice(1).join("\n")),
-    tags: rule.tags,
-  }));
+export default function Home(props: PageProps<RuleData[]>) {
+  const rules = props.data;
 
   const search = props.url.searchParams.get("q") ?? "";
   const allRules = props.url.searchParams.has("all");
@@ -39,35 +41,28 @@ function IndexPage(props: PageProps) {
     .filter((rule: RuleData) => rule.code.includes(search));
 
   return (
-    <div class={tw`dark:bg-[#0d1117] dark:text-white py-6 h-full`}>
-      <div class={tw`mx-auto max-w-screen-md px-6 sm:px-6 md:px-8`}>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/gh/lucacasonato/manual@df7ae27/www/static/markdown.css"
-            crossOrigin="anonymous"
-          />
-          <style>{style}</style>
-        </Head>
+    <div class="py-6">
+      <div class="mx-auto max-w-screen-md px-6 sm:px-6 md:px-8">
+        <CommonHead />
         <Header />
-        <main class={tw`my-8`}>
-          <label for="search" class={tw`sr-only`}>Search</label>
+        <main class="my-8">
+          <label for="search" class="sr-only">Search</label>
           <form id="search_form">
             <input
               type="text"
               name="q"
-              class={tw`w-full border h-10 border-gray-200 dark:border-gray-500 rounded rounded-r-none px-3 relative dark:bg-gray-800 `}
+              class="w-full border h-10 border-gray-200 dark:border-gray-500 rounded rounded-r-none px-3 relative dark:bg-gray-800 "
               placeholder="Search"
               value={search}
             />
-            <div class={tw`mt-2`}>
+            <div class="mt-2">
               <input
                 type="checkbox"
                 id="all_rules"
                 name="all"
                 checked={allRules}
               />
-              <label htmlFor="all_rules" class={tw`ml-2`}>
+              <label htmlFor="all_rules" class="ml-2">
                 Show all rules
               </label>
             </div>
@@ -79,7 +74,7 @@ function IndexPage(props: PageProps) {
             }}
           >
           </script>
-          <div class={tw`mt-6 text-gray-600 dark:text-gray-400`}>
+          <div class="mt-6 text-gray-600 dark:text-gray-400">
             Showing {searchResults.length} out of {rules.length} rules
           </div>
           <div>
@@ -97,27 +92,27 @@ function Rule(props: { rule: RuleData }) {
 
   return (
     <section
-      class={tw`my-8 border-gray-200 dark:border-[#313235] border-2 rounded-lg overflow-hidden`}
+      class="my-8 border-gray-200 dark:border-[#313235] border-2 rounded-lg overflow-hidden"
       id={rule.code}
     >
       <div
-        class={tw`p-3 border-b border-gray-200 flex justify-between flex-wrap gap-2 items-center bg-white dark:bg-[#0d1117] dark:border-[#313235]`}
+        class="p-3 border-b border-gray-200 flex justify-between flex-wrap gap-2 items-center bg-white dark:bg-[#0d1117] dark:border-[#313235]"
       >
-        <h1 class={tw`text-xl font-bold`}>
-          <a href={`#${rule.code}`} class={tw`hover:underline`}>
+        <h1 class="text-xl font-bold">
+          <a href={`#${rule.code}`} class="hover:underline">
             {rule.code}
           </a>
         </h1>
         {rule.tags.includes("recommended") && (
           <span
-            class={tw`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-blue-100 text-blue-800`}
+            class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-blue-100 text-blue-800"
           >
             Recommended
           </span>
         )}
       </div>
       <div
-        class={tw`relative bg-gray-50 dark:bg-[#192029] dark:text-white p-3`}
+        class="relative bg-gray-50 dark:bg-[#192029] dark:text-white p-3"
       >
         {rule.docs.length > 0
           ? (
@@ -126,20 +121,18 @@ function Rule(props: { rule: RuleData }) {
                 <summary>
                   <div
                     dangerouslySetInnerHTML={{ __html: rule.snippet }}
-                    class={"markdown-body " + tw`inline-block`}
+                    class="markdown-body inline-block"
                   />
                 </summary>
                 <div
                   dangerouslySetInnerHTML={{ __html: rule.docs }}
-                  class={"markdown-body " + tw`mt-4`}
+                  class="markdown-body mt-4"
                 />
               </details>
             </>
           )
-          : <div class={tw`text-gray-500 italic`}>no docs available</div>}
+          : <div class="text-gray-500 italic">no docs available</div>}
       </div>
     </section>
   );
 }
-
-export default IndexPage;

www/server_deps.ts
@@ -1,1 +1,0 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/d1076b0ad1b420aec689324b3342e543c0d5a591/server.ts";

www/static/extension.css
@@ -1,0 +1,26 @@
+h3#invalid ~ pre:not(h3#valid ~ pre) {
+  background-color: #f4e7e7;
+  border: 2px solid #ec8282;
+}
+
+h3#valid ~ pre {
+  background-color: #e9f0db;
+  border: 2px solid #72c072;
+}
+
+@media (prefers-color-scheme: dark) {
+  body {
+    background-color: #0d1117;
+    color: #ffffff;
+  }
+
+  h3#invalid ~ pre:not(h3#valid ~ pre) {
+    background-color: #221616;
+    border: 2px solid #aa3f3f;
+  }
+
+  h3#valid ~ pre {
+    background-color: #1b2216;
+    border: 2px solid #406e40;
+  }
+}

www/twind.config.ts
@@ -1,0 +1,5 @@
+import { Options } from "$fresh/plugins/twind.ts";
+
+export default {
+  selfURL: import.meta.url,
+} as Options;

www/utils/render_markdown.ts
@@ -1,8 +1,8 @@
-import { marked, Prism } from "../deps.ts";
+import Prism from "prism";
+import marked from "marked";
 
-export async function diskFetcher(path: string): Promise<string> {
-  return await Deno.readTextFile(path);
-}
+import "https://esm.sh/prismjs@1.25.0/components/prism-javascript.js?no-check&pin=v57";
+import "https://esm.sh/prismjs@1.25.0/components/prism-typescript.js?no-check&pin=v57";
 
 export function renderMarkdown(markdown: string): string {
   const html = marked(markdown, {