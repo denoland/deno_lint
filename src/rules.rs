de74fd3f71ed9664655f5b5d9571e2ed042f2add
Nbiba Bedis <bedisnbiba@gmail.com>
2023-07-16 23:19:00 +0100

    unremove rule

--- src/rules.rs
+++ src/rules.rs
@@ -246,7 +246,7 @@ fn get_all_rules_raw() -> Vec<&'static dyn LintRule> {
     &no_array_constructor::NoArrayConstructor,
     &no_async_promise_executor::NoAsyncPromiseExecutor,
     &no_await_in_loop::NoAwaitInLoop,
-    &no_await_in_loop::NoAwaitInLoop,
+    &no_await_in_sync_fn::NoAwaitInSyncFn,
     &no_case_declarations::NoCaseDeclarations,
     &no_class_assign::NoClassAssign,
     &no_compare_neg_zero::NoCompareNegZero,
