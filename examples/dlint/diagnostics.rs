34f95d205fdad98b2733e27af1616319dec20f3f
David Sherret <dsherret@gmail.com>
2024-02-08 09:20:57 -0500

    Maybe fix

--- examples/dlint/diagnostics.rs
+++ examples/dlint/diagnostics.rs
@@ -16,11 +16,14 @@ pub fn display_diagnostics(
 
 fn print_compact(diagnostics: &[LintDiagnostic]) {
   for diagnostic in diagnostics {
+    let display_index = diagnostic
+      .text_info
+      .line_and_column_display(diagnostic.range.start);
     eprintln!(
-      "{}: line {}, col {}, Error - {} ({})",
-      diagnostic.specifier,
-      diagnostic.range.start.line_index + 1,
-      diagnostic.range.start.column_index + 1,
+      "{}",
+      diagnostic.display(),
+      display_index.line_number,
+      display_index.column_number,
       diagnostic.message,
       diagnostic.code
     )
