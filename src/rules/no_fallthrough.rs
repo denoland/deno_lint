3571addf7dff03f0431b2c997bac5be026632c6b
Koh <shinokou213@gmail.com>
2024-05-13 16:32:32 +0900

    update a comment for the test

--- src/rules/no_fallthrough.rs
+++ src/rules/no_fallthrough.rs
@@ -174,7 +174,7 @@ mod tests {
       "switch('test') { case 'symbol':\n case 'function':\n default: b(); }",
       "switch('test') { case 'symbol': case 'function': default: b(); }",
       "switch(foo) { case 1:\n\n default: a(); }",
-      "switch(foo) { case 1:\n// some comment\n default: a(); }",
+      "switch(foo) { case 1:\n// comment\n default: a(); }",
 
       // https://github.com/denoland/deno_lint/issues/746
       r#"
