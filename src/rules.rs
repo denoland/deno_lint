d6876124b7d0a3f0d2b6658c0c5c56bdbf40ec51
Marvin Hagemeister <marvin@deno.com>
2024-11-18 11:30:15 +0100

    feat: add jsx-no-comment-text-nodes rule

--- src/rules.rs
+++ src/rules.rs
@@ -31,6 +31,7 @@ pub mod jsx_boolean_value;
 pub mod jsx_curly_braces;
 pub mod jsx_key;
 pub mod jsx_no_children_prop;
+pub mod jsx_no_comment_text_nodes;
 pub mod jsx_no_danger_with_children;
 pub mod jsx_no_duplicate_props;
 pub mod jsx_no_useless_fragment;
@@ -274,6 +275,7 @@ fn get_all_rules_raw() -> Vec<Box<dyn LintRule>> {
     Box::new(jsx_curly_braces::JSXCurlyBraces),
     Box::new(jsx_key::JSXKey),
     Box::new(jsx_no_children_prop::JSXNoChildrenProp),
+    Box::new(jsx_no_comment_text_nodes::JSXNoCommentTextNodes),
     Box::new(jsx_no_danger_with_children::JSXNoDangerWithChildren),
     Box::new(jsx_no_duplicate_props::JSXNoDuplicateProps),
     Box::new(jsx_no_useless_fragment::JSXNoUselessFragment),
---
+++ src/rules/jsx_no_comment_text_nodes.rs
@@ -0,0 +1,88 @@
+// Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
+
+use super::{Context, LintRule};
+use crate::handler::{Handler, Traverse};
+use crate::tags::Tags;
+use crate::{tags, Program};
+use deno_ast::view::JSXText;
+use deno_ast::SourceRanged;
+
+#[derive(Debug)]
+pub struct JSXNoCommentTextNodes;
+
+const CODE: &str = "jsx-no-comment-text-nodes";
+
+impl LintRule for JSXNoCommentTextNodes {
+  fn tags(&self) -> Tags {
+    &[tags::RECOMMENDED, tags::REACT, tags::JSX, tags::FRESH]
+  }
+
+  fn code(&self) -> &'static str {
+    CODE
+  }
+
+  fn lint_program_with_ast_view(
+    &self,
+    context: &mut Context,
+    program: Program,
+  ) {
+    JSXNoCommentTextNodesHandler.traverse(program, context);
+  }
+
+  #[cfg(feature = "docs")]
+  fn docs(&self) -> &'static str {
+    include_str!("../../docs/rules/jsx_no_comment_text_nodes.md")
+  }
+}
+
+const MESSAGE: &str =
+  "Comments inside children should be placed inside curly braces";
+
+struct JSXNoCommentTextNodesHandler;
+
+impl Handler for JSXNoCommentTextNodesHandler {
+  fn jsx_text(&mut self, node: &JSXText, ctx: &mut Context) {
+    //
+    let value = &node.inner.value;
+    if value.starts_with("//") || value.starts_with("/*") {
+      ctx.add_diagnostic(node.range(), CODE, MESSAGE);
+    }
+  }
+}
+
+// most tests are taken from ESlint, commenting those
+// requiring code path support
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn jsx_no_comment_text_nodes_valid() {
+    assert_lint_ok! {
+      JSXNoCommentTextNodes,
+      filename: "file:///foo.jsx",
+      // non derived classes.
+      r#"<div>{/* comment */}</div>"#,
+    };
+  }
+
+  #[test]
+  fn jsx_no_comment_text_nodes_invalid() {
+    assert_lint_err! {
+      JSXNoCommentTextNodes,
+      filename: "file:///foo.jsx",
+      "<div>// comment</div>": [
+        {
+          col: 5,
+          message: MESSAGE,
+        }
+      ],
+      r#"<div>/* comment */</div>"#: [
+        {
+          col: 5,
+          message: MESSAGE,
+        }
+      ],
+    };
+  }
+}
