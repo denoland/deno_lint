e9a06e505f63f5a3c3ac2dd972ff4ff0b7a67b11
Marvin Hagemeister <marvin@deno.com>
2024-11-18 13:40:12 +0100

    feat: add jsx_curly_braces rule

--- src/rules.rs
+++ src/rules.rs
@@ -26,6 +26,7 @@ pub mod fresh_server_event_handlers;
 pub mod getter_return;
 pub mod guard_for_in;
 pub mod jsx_boolean_value;
+pub mod jsx_curly_braces;
 pub mod jsx_no_comment_text_nodes;
 pub mod no_array_constructor;
 pub mod no_async_promise_executor;
@@ -261,6 +262,7 @@ fn get_all_rules_raw() -> Vec<Box<dyn LintRule>> {
     Box::new(getter_return::GetterReturn),
     Box::new(guard_for_in::GuardForIn),
     Box::new(jsx_boolean_value::JSXBooleanValue),
+    Box::new(jsx_curly_braces::JSXCurlyBraces),
     Box::new(jsx_no_comment_text_nodes::JSXNoCommentTextNodes),
     Box::new(no_array_constructor::NoArrayConstructor),
     Box::new(no_async_promise_executor::NoAsyncPromiseExecutor),
