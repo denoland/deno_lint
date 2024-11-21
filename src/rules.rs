6d3d519f562e1ab400d3cec06cb53c54f8d25ba1
Marvin Hagemeister <marvin@deno.com>
2024-11-21 15:46:59 +0100

    feat: add jsx-no-useless-fragment rule

--- src/rules.rs
+++ src/rules.rs
@@ -31,6 +31,7 @@ pub mod jsx_key;
 pub mod jsx_no_comment_text_nodes;
 pub mod jsx_no_duplicate_props;
 pub mod jsx_no_target_blank;
+pub mod jsx_no_useless_fragment;
 pub mod no_array_constructor;
 pub mod no_async_promise_executor;
 pub mod no_await_in_loop;
@@ -270,6 +271,7 @@ fn get_all_rules_raw() -> Vec<Box<dyn LintRule>> {
     Box::new(jsx_no_comment_text_nodes::JSXNoCommentTextNodes),
     Box::new(jsx_no_duplicate_props::JSXNoDuplicateProps),
     Box::new(jsx_no_target_blank::JSXNoTargetBlank),
+    Box::new(jsx_no_useless_fragment::JSXNoUselessFragment),
     Box::new(no_array_constructor::NoArrayConstructor),
     Box::new(no_async_promise_executor::NoAsyncPromiseExecutor),
     Box::new(no_await_in_loop::NoAwaitInLoop),
