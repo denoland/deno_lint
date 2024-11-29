37f52831d0e0eed7e9c667d79b2374cb72802fb3
Marvin Hagemeister <marvin@deno.com>
2024-11-29 11:13:55 +0100

    feat: add jsx-void-dom-elements-no-children rule (#1347)
    * feat: add jsx-void-dom-elements-no-children rule
    
    * fix: update docs
    
    * fix: update schemas
    
    * update rule docs
    
    Co-authored-by: Bartek Iwańczuk <biwanczuk@gmail.com>
    
    * add recommended
    
    Co-authored-by: Bartek Iwańczuk <biwanczuk@gmail.com>
    
    * chore: fmt
    
    * chore: update docs
    
    ---------
    
    Co-authored-by: Bartek Iwańczuk <biwanczuk@gmail.com>

--- src/rules.rs
+++ src/rules.rs
@@ -28,6 +28,7 @@ pub mod jsx_curly_braces;
 pub mod jsx_no_children_prop;
 pub mod jsx_no_duplicate_props;
 pub mod jsx_props_no_spread_multi;
+pub mod jsx_void_dom_elements_no_children;
 pub mod no_array_constructor;
 pub mod no_async_promise_executor;
 pub mod no_await_in_loop;
@@ -264,6 +265,7 @@ fn get_all_rules_raw() -> Vec<Box<dyn LintRule>> {
     Box::new(jsx_no_children_prop::JSXNoChildrenProp),
     Box::new(jsx_no_duplicate_props::JSXNoDuplicateProps),
     Box::new(jsx_props_no_spread_multi::JSXPropsNoSpreadMulti),
+    Box::new(jsx_void_dom_elements_no_children::JSXVoidDomElementsNoChildren),
     Box::new(no_array_constructor::NoArrayConstructor),
     Box::new(no_async_promise_executor::NoAsyncPromiseExecutor),
     Box::new(no_await_in_loop::NoAwaitInLoop),
