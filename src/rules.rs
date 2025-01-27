da1000caef89547619856124feb7b2c5ecd36f87
Marvin Hagemeister <marvin@deno.com>
2025-01-27 11:20:07 +0100

    fix: rename `jsx-no-danger-with-children` -> `react-no-danger-with-children`

--- src/rules.rs
+++ src/rules.rs
@@ -273,7 +273,7 @@ fn get_all_rules_raw() -> Vec<Box<dyn LintRule>> {
     Box::new(jsx_key::JSXKey),
     Box::new(jsx_no_children_prop::JSXNoChildrenProp),
     Box::new(jsx_no_comment_text_nodes::JSXNoCommentTextNodes),
-    Box::new(jsx_no_danger_with_children::JSXNoDangerWithChildren),
+    Box::new(jsx_no_danger_with_children::ReactNoDangerWithChildren),
     Box::new(jsx_no_duplicate_props::JSXNoDuplicateProps),
     Box::new(jsx_no_unescaped_entities::JSXNoUnescapedEntities),
     Box::new(jsx_no_useless_fragment::JSXNoUselessFragment),
